7b3bb502d16a130c1e11cf2a4e40a5a5303bdd6f
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-01-08 11:51:24 +0100

    Add new version of py-lxml and add setuptools as run dependency

--- var/spack/repos/builtin/packages/placement-algorithm/package.py
+++ var/spack/repos/builtin/packages/placement-algorithm/package.py
@@ -17,7 +17,7 @@ class PlacementAlgorithm(PythonPackage):
 
     build_directory = 'python'
 
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build','run'))
 
     depends_on('py-lxml', type='run')
     depends_on('py-numpy', type='run')
