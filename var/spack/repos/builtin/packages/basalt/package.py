650c5c7f5e9c7d0d8e8e1e324b861dc8ef029775
iomaganaris <iomagkanaris@gmail.com>
2019-02-13 19:14:46 +0100

    Modified basalt spack package.py and added py-humanize package.py
    - Deleted commented lines of code in both of the packages
    - Changed the basalt package discription
    - Made version v0.1.1 as preferred in installation of basalt package
    - Renamed humanize package to py-humanize package, as this is a python package
    - Py-humanize package is the same added in the py-humanize PR

--- var/spack/repos/builtin/packages/basalt/package.py
+++ var/spack/repos/builtin/packages/basalt/package.py
@@ -3,34 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install basalt
-#
-# You can edit this file again by typing:
-#
-#     spack edit basalt
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 
 
 class Basalt(PythonPackage):
-    """C++11 Graph Storage library"""
+    """C++11 Graph Storage library with Python interface"""
 
     homepage = "https://github.com/tristan0x/basalt"
     url      = "git@github.com:tristan0x/basalt.git"
     
-    version('develop', git=url, branch='master', submodules=True, preferred=True)
-    version('0.1.1', git=url, tag='v0.1.1', submodules=True)
+    version('develop', git=url, branch='master', submodules=True)
+    version('0.1.1', git=url, tag='v0.1.1', submodules=True, preferred=True)
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('rocksdb~static')
@@ -40,6 +23,6 @@ class Basalt(PythonPackage):
     depends_on('py-docopt', type=('build', 'run'))
     depends_on('py-cached-property', type=('build', 'run'))
     depends_on('py-h5py', type=('build', 'run'))
-    depends_on('humanize', type=('build', 'run'))
+    depends_on('py-humanize', type=('build', 'run'))
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('benchmark')
