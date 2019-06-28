7db19b98d8b046606e624a49fb2278d356e98a3c
Fernando Pereira <fernando.pereira@epfl.ch>
2019-06-28 14:55:14 +0200

    Neurodamus-core: +common buildngs mods
    Reporting lib disabled

--- var/spack/repos/builtin/packages/neurodamus-core/package.py
+++ var/spack/repos/builtin/packages/neurodamus-core/package.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import llnl.util.tty as tty
 
+
 class NeurodamusCore(Package):
     """Library of channels developed by Blue Brain Project, EPFL"""
 
@@ -18,6 +19,9 @@ class NeurodamusCore(Package):
 
     variant('python', default=False, description="Enable Python Neurodamus")
     variant('common', default=False, description="Merge in synapse mechanisms hoc & mods")
+
+    # Attempt to support building
+    depends_on('neuron~binary', when='+common')
 
     # Neurodamus py is currently an extension to core
     resource(name='pydamus',
@@ -42,17 +46,44 @@ class NeurodamusCore(Package):
         shutil.copytree('mod', prefix.mod)
         if spec.satisfies('+python'):
             copy_tree('resources/neurodamus-py', prefix.python)
-        if spec.satisfies('+common'):
-            copy_all('resources/common/hoc', prefix.hoc)
-            copy_all('resources/common/mod', prefix.mod)
 
         filter_file(r'UNKNOWN_CORE_VERSION', r'%s' % spec.version, prefix.hoc.join('defvar.hoc'))
-
         try:
             commit_hash = self.fetcher[0].get_commit()
             filter_file(r'UNKNOWN_CORE_HASH', r'\'%s\'' % commit_hash, prefix.hoc.join('defvar.hoc'))
         except Exception as e:
-            tty.warn(e)
+            tty.warn(str(e))
+
+        # +Common will bring common mods and build a bare nrnmechlib
+        if spec.satisfies('+common'):
+            copy_all('resources/common/hoc', prefix.hoc)
+            copy_all('resources/common/mod', prefix.mod)
+
+            with working_dir(prefix):
+                which('nrnivmodl')('-incflags', '-DDISABLE_REPORTINGLIB', 'mod')
+
+                bindir = os.path.basename(self.neuron_archdir)
+                special = join_path(bindir, 'special')
+                open('.bindir', 'w').write(bindir)
+
+                if not os.path.isfile(special):
+                    raise Exception("Failed to build neurodamus core mods")
+
+                # Cleanup
+                for f in find(bindir, '*.lo'): os.remove(f)
+                for f in find(bindir, '*.mod'): os.remove(f)
+                for f in find(bindir + '/.libs', '*.o'): os.remove(f)
+                os.mkdir(bindir + "/mod2c")
+                for f in find(bindir, "*.c*"): shutil.move(f, bindir + "/mod2c/")
 
     def setup_environment(self, spack_env, run_env):
         run_env.set('HOC_LIBRARY_PATH', self.prefix.hoc)
+        if self.spec.satisfies('+common'):
+            run_env.set('MOD_LIBRARY_PATH', self.prefix.mod)
+            bindir_info = self.prefix.join('.bindir')
+            if os.path.isfile(bindir_info):
+                bindir = open(bindir_info, 'r').readline()
+                mechlib = find_libraries('libnrnmech', join_path(self.prefix, bindir, '.libs'))[0]
+                run_env.set('NRNMECH_LIB_PATH', mechlib)
+            else:
+                tty.warn("No .bindir info file found. NRNMECH_LIB_PATH env var wont be set")
