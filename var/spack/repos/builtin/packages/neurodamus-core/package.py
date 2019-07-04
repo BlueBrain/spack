ad29516e6026e1a287891d8354f0570e199f7081
Fernando Pereira <fernando.pereira@epfl.ch>
2019-07-04 13:04:49 +0200

    Aiin new version neurodamus core 2.4.0

--- var/spack/repos/builtin/packages/neurodamus-core/package.py
+++ var/spack/repos/builtin/packages/neurodamus-core/package.py
@@ -13,6 +13,7 @@ class NeurodamusCore(Package):
     git      = "ssh://bbpcode.epfl.ch/sim/neurodamus-core"
 
     version('develop', git=git, branch='master', clean=False)
+    version('2.4.0', git=git, tag='2.4.0', clean=False)
     version('2.3.4', git=git, tag='2.3.4', clean=False)
     version('2.3.3', git=git, tag='2.3.3', clean=False)
     version('2.2.1', git=git, tag='2.2.1', clean=False)
