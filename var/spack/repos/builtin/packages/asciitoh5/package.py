0efedd8ef8802063b9f51e8a0be6e80726c74ed4
Antonio Bellotta <antonio.bellotta@epfl.ch>
2019-08-02 14:50:31 +0200

    updated modules.yaml and bbp-packages.yaml for asciitoh5

--- var/spack/repos/builtin/packages/asciitoh5/package.py
+++ var/spack/repos/builtin/packages/asciitoh5/package.py
@@ -12,9 +12,7 @@ class Asciitoh5(Package):
     git      = "ssh://bbpcode.epfl.ch/sim/utils/asciitoh5"
 
     version('develop', git=git, branch='master')
-    version('0.4', git=git, tag='0.4')
-    version('0.2', git=git, tag='0.2')
-    version('0.1', git=git, tag='0.1')
+    version('1.0', git=git, tag='1.0')
 
     depends_on('neuron~mpi')
     depends_on('hdf5~mpi')
@@ -23,16 +21,14 @@ class Asciitoh5(Package):
 	os.mkdir(prefix.lib)
 	shutil.copytree('hoc', prefix.lib.hoc)
         shutil.copytree('mod', prefix.lib.mod)
+	shutil.copytree('bin', prefix.bin)
 	with working_dir(prefix):
 	    link_flag = spec['hdf5'].libs.rpath_flags + ' ' + spec['hdf5'].libs.ld_flags
 	    include_flag = ' -I%s' % (spec['hdf5'].prefix.include)
             which('nrnivmodl')('-incflags', include_flag, '-loadflags', link_flag, 'lib/mod')
 	    bindir = os.path.basename(self.neuron_archdir)
             special = join_path(bindir, 'special')
-            os.mkdir(prefix.bin)
 	    shutil.copy(special, prefix.bin)
-            for f in find('lib/hoc/neuronHDF5', 'convert.sh'):
-                shutil.copy(f, prefix.bin)
 
     def setup_environment(self, spack_env, run_env):
-        run_env.set('HOC_LIBRARY_PATH', self.prefix.lib)
+        run_env.set('HOC_LIBRARY_PATH', self.prefix.lib.hoc)
