83ac613b0fdaa09aa2484d51277b08f54d6f7b66
genrich <genrich.ivaska@epfl.ch>
2019-08-05 13:45:33 +0200

    fix py-interval url

--- var/spack/repos/builtin/packages/py-interval/package.py
+++ var/spack/repos/builtin/packages/py-interval/package.py
@@ -10,7 +10,7 @@ class PyInterval(PythonPackage):
     """Python interval and interval set implementation."""
 
     homepage = "https://pypi.org/project/interval/1.0.0/"
-    url      = "https://files.pythonhosted.org/packages/b3/2d/b337afbd232ea1ea9c38401135054bf763e7930ea5e2e49bc39af35c3443/interval-1.0.0.tar.bz2"
+    url      = "https://pypi.io/packages/source/i/interval/interval-1.0.0.tar.bz2"
 
     version('1.0.0', '9dd55d218911e2b3c9cc9755608aeaf6')
 
