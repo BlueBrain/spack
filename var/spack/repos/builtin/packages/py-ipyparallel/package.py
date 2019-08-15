66270bfec0e6f18b48090ed2822d967184065d7e
Matthias Wolf <matthias.wolf@epfl.ch>
2019-08-15 08:58:22 +0200

    py-ipyparallel: Python 2 must die!

--- var/spack/repos/builtin/packages/py-ipyparallel/package.py
+++ var/spack/repos/builtin/packages/py-ipyparallel/package.py
@@ -34,12 +34,11 @@ class PyIpyparallel(PythonPackage):
     version('6.2.4', sha256='76c7b028962b0ba762e4e45b450ee3a4353e7221526a8af812e817d7ef6ac065')
     version('6.2.2', sha256='02b225966d5c20f12b1fba0b6b10aa5d352a6b492e075f137ff0ff6e95b9358e')
 
+    depends_on('python@3.6:')
+
     depends_on('py-setuptools', type='build')
     depends_on('py-tornado', type='run')
     depends_on('py-traitlets', type='run')
     depends_on('py-zmq', type='run')
-    depends_on('py-ipython@:6.999', type='run', when='^python@:2.999')
-    depends_on('py-ipython', type='run', when='^python@3.6.5')
-    depends_on('py-ipykernel@:4.999', type='run', when='^python@:2.999')
-    depends_on('py-ipykernel', type='run', when='^python@3:')
-    depends_on('py-futures', type='run', when='^python@:2.999')
+    depends_on('py-ipython', type='run')
+    depends_on('py-ipykernel', type='run')
