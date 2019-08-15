e4d5fa0b4ebf83e89b7b13ce0faa624df5025d7d
Matthias Wolf <matthias.wolf@epfl.ch>
2019-08-15 09:12:39 +0200

    py-ipyparallel: fix dependencies

--- var/spack/repos/builtin/packages/py-ipyparallel/package.py
+++ var/spack/repos/builtin/packages/py-ipyparallel/package.py
@@ -5,22 +5,6 @@
 # This file is part of Spack.
 # Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
 # LLNL-CODE-647188
-#
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
 
@@ -34,11 +18,13 @@ class PyIpyparallel(PythonPackage):
     version('6.2.4', sha256='76c7b028962b0ba762e4e45b450ee3a4353e7221526a8af812e817d7ef6ac065')
     version('6.2.2', sha256='02b225966d5c20f12b1fba0b6b10aa5d352a6b492e075f137ff0ff6e95b9358e')
 
-    depends_on('python@3.6:')
-
     depends_on('py-setuptools', type='build')
     depends_on('py-tornado', type='run')
     depends_on('py-traitlets', type='run')
     depends_on('py-zmq', type='run')
+    depends_on('py-zmq@:16', type='run', when='^python@:2.7')
     depends_on('py-ipython', type='run')
+    depends_on('py-ipython@:6.999', type=('build', 'run'), when='^python@:2.7')
     depends_on('py-ipykernel', type='run')
+    depends_on('py-ipykernel@:4.999', type='run', when='^python@:2.7')
+    depends_on('py-futures', type='run', when='^python@:2.7')
