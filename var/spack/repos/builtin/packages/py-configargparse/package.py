d46b9e47c5a701ba283c4ce88cb2d002d18a1376
Matthias Wolf <matthias.wolf@epfl.ch>
2019-09-10 10:53:15 +0200

    Break line.

--- var/spack/repos/builtin/packages/py-configargparse/package.py
+++ var/spack/repos/builtin/packages/py-configargparse/package.py
@@ -7,8 +7,8 @@ from spack import *
 
 
 class PyConfigargparse(PythonPackage):
-    """A drop-in replacement for argparse that allows options to also be set via config files and/or environment variables.
-    """
+    """A drop-in replacement for argparse that allows options to also be
+    set via config files and/or environment variables."""
 
     homepage = "https://github.com/bw2/ConfigArgParse"
     url      = "https://pypi.io/packages/source/C/ConfigArgParse/ConfigArgParse-3.11.2.tar.gz"
