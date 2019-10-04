b16cb6a188ea7d275c50de77b960fa31ce878ce2
Pawel Podhajski <pawel.podhajski@epfl.ch>
2019-10-04 17:11:09 +0200

    remove GLM (exist as a submoule)

--- var/spack/repos/builtin/packages/brayns/package.py
+++ var/spack/repos/builtin/packages/brayns/package.py
@@ -40,7 +40,6 @@ class Brayns(CMakePackage):
     depends_on('glew', when='+viewer')
     depends_on('libarchive')
     depends_on('cgal')
-    depends_on('glm')
     depends_on('libjpeg-turbo', when='+net')
     depends_on('libuv', when='+net')
     depends_on('opengl', when='+viewer')
