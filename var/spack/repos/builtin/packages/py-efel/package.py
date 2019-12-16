686c0f1fc85c1e61527dd5931ec2f2c5a08e7ee1
Benoit Coste <benoit.coste@epfl.ch>
2019-12-16 16:03:04 +0100

    Fix hash

--- var/spack/repos/builtin/packages/py-efel/package.py
+++ var/spack/repos/builtin/packages/py-efel/package.py
@@ -37,7 +37,7 @@ class PyEfel(PythonPackage):
     homepage = "https://github.com/BlueBrain/eFEL"
     url = "https://pypi.io/packages/source/e/efel/efel-3.0.70.tar.gz"
 
-    version('3.0.70')
+    version('3.0.70', sha256='3f3368012cdec5ca7d5551cea35b30a53befd0c0c740fc535209f840616c07b1')
     version('3.0.22', sha256='97b2c1a0425b12cd419e8539bb1e936ce64c4e93f5d0dd7f81f38554490064a2')
 
     depends_on('py-setuptools', type='build')
