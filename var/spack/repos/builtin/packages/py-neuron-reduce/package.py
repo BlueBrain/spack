0adb60de70841d0ece0765b5007448c8f792093e
Sanin Aleksei <aleksei.sanin@efpl.ch>
2020-02-20 11:19:02 +0100

    add v0.0.6 version

--- var/spack/repos/builtin/packages/py-neuron-reduce/package.py
+++ var/spack/repos/builtin/packages/py-neuron-reduce/package.py
@@ -14,6 +14,7 @@ class PyNeuronReduce(PythonPackage):
     git = "git@github.com:orena1/neuron_reduce.git"
 
     version('develop', branch='master')
+    version('v0.0.6', sha256='0c0071f499d2663aa18b8cf1c86c4a3d5a1c3c85')
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-numpy@1.17:', type='run')
