30fb6108800c16a73a9933387f854305d3d353fa
Omar Awile <omar.awile@epfl.ch>
2020-03-11 15:06:12 +0100

    Added distmesh option and dependencies on Omega_h

--- var/spack/repos/builtin/packages/steps/package.py
+++ var/spack/repos/builtin/packages/steps/package.py
@@ -20,6 +20,7 @@ class Steps(CMakePackage):
 
     variant("native", default=True, description="Generate non-portable arch-specific code")
     variant("lapack", default=False, description="Use new BDSystem/Lapack code for E-Field solver")
+    variant("distmesh", default=False, description="Add solvers based on distributed mesh")
     variant("petsc", default=False, description="Use PETSc library for parallel E-Field solver")
     variant("mpi", default=True, description="Use MPI for parallel solvers")
     variant("coverage", default=False, description="Enable code coverage")
@@ -38,6 +39,7 @@ class Steps(CMakePackage):
     depends_on("py-unittest2", type=("build", "test"))
     depends_on("python")
 
+    depends_on("omega-h", when="-bundle+distmesh")
     depends_on("easyloggingpp", when="~bundle")
     depends_on("random123", when="~bundle")
     depends_on("sundials@:2.99.99+int64", when="~bundle")
@@ -55,6 +57,8 @@ class Steps(CMakePackage):
             ]
             for bundle in bundles:
                 args.append("-DUSE_BUNDLE_{0}:BOOL=OFF".format(bundle))
+            if "+distmesh" in spec:
+                args.append("-DUSE_BUNDLE_Omega_h:BOOL=OFF")
 
         if "+native" in spec:
             args.append("-DTARGET_NATIVE_ARCH:BOOL=True")
@@ -78,6 +82,11 @@ class Steps(CMakePackage):
 
         if "+coverage" in spec:
             args.append("-DENABLE_CODECOVERAGE:BOOL=True")
+
+        if "+distmesh" in spec:
+            args.append("-DENABLE_DISTRIBUTED_MESH:BOOL=True")
+        else:
+            args.append("-DENABLE_DISTRIBUTED_MESH:BOOL=False")
 
         args.append('-DBLAS_LIBRARIES=' + spec['blas'].libs.joined(";"))
         args.append('-DPYTHON_EXECUTABLE='
