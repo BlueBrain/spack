8bd606d7264ef70172f74f1562f6fe1f7c96c7d7
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-05-13 17:51:52 +0200

    Update neuron to latest master

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -30,7 +30,7 @@ class Neuron(CMakePackage):
     patch("apply_79a4d2af_load_balance_fix.patch", when="@7.8.0b")
 
     version("develop", branch="master")
-    version("7.8.0d",  branch="bin_special", preferred=True)
+    version("7.8.0d",  branch="defabcd", preferred=True)
     version("7.8.0c",  commit="81f9e60")
     version("7.8.0b",  commit="92a208b")
     version("7.6.8",   tag="7.6.8")
