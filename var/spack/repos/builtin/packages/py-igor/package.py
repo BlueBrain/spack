d308fc10e39c0bb08533df1d659fe7bb9da2e45d
damart <tanguy.damart@epfl.ch>
2020-07-21 16:50:01 +0200

    Add bluepyefe and igor packages

---
+++ var/spack/repos/builtin/packages/py-igor/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyIgor(PythonPackage):
+    """igor: interface for reading binary IGOR files."""
+
+    homepage = "http://blog.tremily.us/posts/igor/"
+    url = "https://pypi.io/packages/source/i/igor/igor-0.3.tar.gz"
+
+    version('0.3', branch='master')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-numpy', type='run')
+    depends_on('py-nose', type='run')
+    depends_on('py-matplotlib', type='run')
