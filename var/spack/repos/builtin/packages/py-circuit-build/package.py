3e2164508bd17a7487da25fbaec944af5dad78c1
aleksei sanin <aleksei.sanin@epfl.ch>
2020-09-02 15:24:02 +0200

    create py-circuit-build package

---
+++ var/spack/repos/builtin/packages/py-circuit-build/package.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyCircuitBuild(PythonPackage):
+    """Pythonic API for building circuits"""
+
+    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/common/circuit-build"
+    git      = "ssh://bbpcode.epfl.ch/common/circuit-build"
+
+    version('develop', branch='master')
+    version('3.1.0', tag='circuit-build-v3.1.0')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+
+    depends_on('py-click@7.0:', type='run')
+    depends_on('snakemake@5.6:', type='run')
+    depends_on('py-pyyaml@5.0:', type='run')
+
