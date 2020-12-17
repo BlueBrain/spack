3bd39101e72ae8bcdd1114a64ef1d3a4a6f268a1
gcastigl <giacomo.castiglioni@epfl.ch>
2020-12-17 18:29:14 +0100

    removed tabs

--- var/spack/repos/builtin/packages/arm-forge/package.py
+++ var/spack/repos/builtin/packages/arm-forge/package.py
@@ -19,9 +19,9 @@ class ArmForge(Package):
     #       versions (and checksums) based on the target platform shows up
 
     version(
-	"20.2.0-linux-rhel7-x86_64",
- 	sha256="26592a77c42f970f724f15b70cc5ce6af1078fd0ef9243a37c3215916cfa7cf4",
-	url="https://content.allinea.com/downloads/arm-forge-20.2-Redhat-7.0-x86_64.tar",
+        "20.2.0-linux-rhel7-x86_64",
+        sha256="26592a77c42f970f724f15b70cc5ce6af1078fd0ef9243a37c3215916cfa7cf4",
+        url="https://content.allinea.com/downloads/arm-forge-20.2-Redhat-7.0-x86_64.tar",
     )
 
     version(
