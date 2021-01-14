30a4ff3d7c2d846a3bbff5dcb075db614694ebec
Ioannis Magkanaris <iomagkanaris@gmail.com>
2021-01-14 16:22:51 +0100

    Fix CC and CXX filtering on nrnivmodl_makefile_cmake.in

--- var/spack/repos/builtin/packages/neuron/package.py
+++ var/spack/repos/builtin/packages/neuron/package.py
@@ -440,11 +440,9 @@ class Neuron(CMakePackage):
                 filter_file(env["CC"], "cc", libtool_makefile, **kwargs)
                 filter_file(env["CXX"], "CC", libtool_makefile, **kwargs)
 
-        # nrnmech_makefile exists in both cmake and autotools buildsi
-        if self.spec.satisfies("+cmake"):
-            assign_operator = "?="
-        else:
-            assign_operator = "="
+        # The assign_operaton should follow any changes done in "bin/nrnivmodl_makefile_cmake.in"
+        # and "bin/nrnmech_makefile.in" when assigning CC and CXX variables
+        assign_operator = "="
         filter_file("CC {0} {1}".format(assign_operator, env["CC"]),
                     "CC = {0}".format(cc_compiler),
                     nrnmech_makefile,
