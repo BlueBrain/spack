07a9abfda067e4e733640c0399c2a4b3a58e88b2
Matthias Wolf <matthias.wolf@epfl.ch>
2021-01-26 17:34:11 +0100

    Add blah to py

--- lib/spack/spack/util/module_cmd.py
+++ lib/spack/spack/util/module_cmd.py
@@ -53,6 +53,13 @@ def module(*args):
         # dump SPACKIGNORE as a placeholder for parsing if LD_LIBRARY_PATH null
         module_cmd += 'echo "${SPACK_NEW_LD_LIBRARY_PATH:-SPACKIGNORE}"'
 
+        # Bash allows to export functions into the environment, and
+        # subprocesses executing Bash can make use of these.
+        #
+        # If the user runs a different shell, emulate the exported module
+        # function to have the module commands work properly.
+        #
+        # Added caveat: `modulecmd` needs to be in PATH.
         environ = dict(os.environ)
         if "BASH_FUNC_module()" not in environ:
             environ["BASH_FUNC_module()"] = "() { eval `modulecmd bash $*`\n}"
@@ -85,6 +92,7 @@ def module(*args):
             os.environ['LD_LIBRARY_PATH'] = new_ld_library_path
 
     else:
+        # See note above
         environ = dict(os.environ)
         if "BASH_FUNC_module()" not in environ:
             environ["BASH_FUNC_module()"] = "() { eval `modulecmd bash $*`\n}"
