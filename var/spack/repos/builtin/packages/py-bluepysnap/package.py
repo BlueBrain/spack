cd46e4fe5dc9c57b7eb0e2a5c10194bbc92c5b2a
tomdele <thomas.delemontex@epfl.ch>
2021-02-04 11:01:46 +0100

    Fix missing deps for bluepysnap

--- var/spack/repos/builtin/packages/py-bluepysnap/package.py
+++ var/spack/repos/builtin/packages/py-bluepysnap/package.py
@@ -25,12 +25,14 @@ class PyBluepysnap(PythonPackage):
     version('0.1.2', sha256='aa29c1344258c1ca0cf7f5f53f3080025b9d098e3366872369586ad7ccf334a2')
 
     depends_on('py-setuptools', type=('build', 'run'))
-
     depends_on('py-functools32', type='run', when='^python@:3.1.99')
-    depends_on('py-cached-property@1.0:', type='run')
+
+    depends_on('py-pandas@0.24.0:', type='run')
     depends_on('py-libsonata@0.1.4:', type='run')
     depends_on('py-numpy@1.8:', type='run')
     depends_on('py-click@7.0:', type='run')
+
+    depends_on('py-cached-property@1.0:', type='run')
     depends_on('py-more-itertools@7.2.0:', type='run')
 
     # Version 0.8.0 is the switch between the python2+python3
