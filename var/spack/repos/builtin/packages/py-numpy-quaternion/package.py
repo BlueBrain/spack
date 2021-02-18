18ee36afefab50d72038f0660c7f9bc04c88891d
Tristan Carel <tristan.carel@epfl.ch>
2021-02-18 09:29:54 +0100

    fixup! py-numpy-quaternion: new package starting at 2020.11.2.17.0.49

--- var/spack/repos/builtin/packages/py-numpy-quaternion/package.py
+++ var/spack/repos/builtin/packages/py-numpy-quaternion/package.py
@@ -17,5 +17,4 @@ class PyNumpyQuaternion(PythonPackage):
     version('2020.11.2.17.0.49', sha256='f65547201fdfa41590b72cfd6ed573e1e9201ca15f19dad429efb8e70e0a4d39')
 
     depends_on('py-setuptools', type='build')
-    depends_on('py-numpy', type=('build', 'run')
-)
+    depends_on('py-numpy', type=('build', 'run'))
