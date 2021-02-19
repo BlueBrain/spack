cdbb8cbfd7070f23a60525d1869f68c88acdca0a
Benoît Coste <benoit.coste@epfl.ch>
2021-02-19 12:45:22 +0100

    Bump workflow

--- var/spack/repos/builtin/packages/py-morph-tool/package.py
+++ var/spack/repos/builtin/packages/py-morph-tool/package.py
@@ -14,19 +14,14 @@ class PyMorphTool(PythonPackage):
     url      = "https://pypi.io/packages/source/m/morph-tool/morph-tool-0.2.5.tar.gz"
 
     version('develop', branch='master')
-    version('2.2.21', sha256='37d932b8b9561d9b5b4d0d3cdb1b146e9d4e3443526b8ef6e88aeeac36528c7e')
-    version('0.2.10', sha256='e46835b9c25532cbee070141ad9f7d3d47109d5473074b47aad08f51bdc40157')
-    version('0.2.5', sha256='7157039a7a421cfbdb75fd305975a6e6ce7667177dc68bd299645b62389622be')
-    version('0.2.3', sha256='767effaa4d2e8c7dfee878c6d48c6647f29b19fee6f790213783c04d8951fee3')
-    version('0.2.1', tag='morph-tool-v0.2.1', git='ssh://bbpcode.epfl.ch/nse/morph-tool')
-    version('0.1.14', tag='morph-tool-v0.1.14', git='ssh://bbpcode.epfl.ch/nse/morph-tool')
+    version('2.3.1', sha256='37d932b8b9561d9b5b4d0d3cdb1b146e9d4e3443526b8ef6e88aeeac36528c7e')
 
     depends_on('py-setuptools', type=('build', 'run'))
     depends_on('neuron+python', when='@0.2.1:', type='run')
     depends_on('py-bluepyopt@1.9.37:', type='run', when='@0.2.1:')
     depends_on('py-click@7.0:', type='run')
     depends_on('py-functools32', when='@:0.2.3^python@:2.99', type='run')
-    depends_on('py-morphio@2.3.4:', type='run')
+    depends_on('py-morphio@2.7.0:', type='run')
     depends_on('py-neurom@1.4.15:', type='run', when='@0.1.14:')
     depends_on('py-pathlib2@2.3.5:', when='@:0.2.3^python@:2.99', type='run')
     depends_on('py-pyyaml', when='@:0.1.14', type='run')
--- var/spack/repos/builtin/packages/py-neuror/package.py
+++ var/spack/repos/builtin/packages/py-neuror/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -10,15 +10,18 @@ from spack import *
 class PyNeuror(PythonPackage):
     """Python morphology manipulation toolkit"""
 
-    homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/nse/morph-repair"
-    git      = "ssh://bbpcode.epfl.ch/nse/morph-repair"
+    homepage = "https://github.com/BlueBrain/NeuroR"
+    git = "https://github.com/BlueBrain/NeuroR.git"
+    url = "https://pypi.io/packages/source/n/neuror/neuror-1.1.11.tar.gz"
 
     version('develop', branch='master')
-    version('1.0.0', tag='NeuroR-v1.0.0')
+    version('1.1.11', tag='NeuroR-v1.1.11')
 
     depends_on('py-setuptools', type=('build', 'run'))
 
     depends_on('py-morph-tool', type='run')
-    depends_on('py-pandas', type='run')
-    depends_on('py-pyquaternion', type='run')
+    depends_on('py-pandas@1.0.3:', type='run')
+    depends_on('py-pyquaternion@0.9.2:', type='run')
     depends_on('py-plotly-helper', type='run')
+    depends_on('py-neurom@mut_morphio', type='run')
+    depends_on('py-morphio@2.7.0:', type='run')
