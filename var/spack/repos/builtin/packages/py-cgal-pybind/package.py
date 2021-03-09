8f355cf9e3a845faf680d5d5b1e53c4c667dc4e5
lguyot <luc.guyot@epfl.ch>
2021-03-09 14:37:59 +0100

    (FIX) Removes superfluous version 0.0.1

--- var/spack/repos/builtin/packages/py-cgal-pybind/package.py
+++ var/spack/repos/builtin/packages/py-cgal-pybind/package.py
@@ -13,7 +13,6 @@ class PyCgalPybind(PythonPackage):
     git      = "ssh://bbpcode.epfl.ch/common/cgal-pybind"
 
     version('develop', submodules=True)
-    version('0.0.1', commit='4805c2e4c82957eb1792290941d8253394a887a4', submodules=True)
     version('0.0.2', commit='7aa1382d1628ccd51f692750a2b145b1df0694d9', submodules=True)
 
     depends_on('py-setuptools', type='build')
