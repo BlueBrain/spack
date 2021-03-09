b2f53044c56ea5922f6d80e295af81621ddcfd79
lguyot <luc.guyot@epfl.ch>
2021-03-09 14:25:28 +0100

    (FIX) Reinstate the  version as it is useful for testing

--- var/spack/repos/builtin/packages/py-cgal-pybind/package.py
+++ var/spack/repos/builtin/packages/py-cgal-pybind/package.py
@@ -7,11 +7,12 @@ from spack import *
 
 
 class PyCgalPybind(PythonPackage):
-    """Internal Python bindgins for CGAL"""
+    """Internal Python bindings for CGAL"""
 
     homepage = "https://bbpcode.epfl.ch/browse/code/common/cgal-pybind/tree/"
     git      = "ssh://bbpcode.epfl.ch/common/cgal-pybind"
 
+    version('develop', submodules=True)
     version('0.0.1', commit='4805c2e4c82957eb1792290941d8253394a887a4', submodules=True)
     version('0.0.2', commit='7aa1382d1628ccd51f692750a2b145b1df0694d9', submodules=True)
 
