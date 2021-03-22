be7afc6a7f004671794562c1eb271af21e1c4997
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2021-03-22 15:45:29 +0100

    Restore mpi variant in py-morphio

--- var/spack/repos/builtin/packages/py-morphio/package.py
+++ var/spack/repos/builtin/packages/py-morphio/package.py
@@ -22,6 +22,8 @@ class PyMorphio(PythonPackage):
     version('2.1.2', tag='v2.1.2', submodules=True, get_full_repo=True)
     version('2.0.8', tag='v2.0.8', submodules=True, get_full_repo=True)
 
+    variant('mpi', default=True, description="Use parallel HDF5 when needed")
+
     depends_on('py-setuptools', type='build')
     depends_on('py-setuptools-scm', type='build')
 
