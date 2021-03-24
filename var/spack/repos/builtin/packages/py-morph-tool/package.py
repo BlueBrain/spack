33ffb504614e468f0d3013c8519c5129a69e8bb4
Mike Gevaert <michael.gevaert@epfl.ch>
2021-03-24 21:23:36 +0100

    bump py-morph-tool & deps

--- var/spack/repos/builtin/packages/py-morph-tool/package.py
+++ var/spack/repos/builtin/packages/py-morph-tool/package.py
@@ -14,6 +14,7 @@ class PyMorphTool(PythonPackage):
     url      = "https://pypi.io/packages/source/m/morph-tool/morph-tool-0.2.5.tar.gz"
 
     version('develop', branch='master')
+    version('2.4.0', sha256='268915ad48f2a2475bbc4685bd1a3610f22978fee1cb4d376d87f45ae4a028e1')
     version('2.2.21', sha256='37d932b8b9561d9b5b4d0d3cdb1b146e9d4e3443526b8ef6e88aeeac36528c7e')
     version('0.2.10', sha256='e46835b9c25532cbee070141ad9f7d3d47109d5473074b47aad08f51bdc40157')
     version('0.2.5', sha256='7157039a7a421cfbdb75fd305975a6e6ce7667177dc68bd299645b62389622be')
@@ -26,8 +27,8 @@ class PyMorphTool(PythonPackage):
     depends_on('py-bluepyopt@1.9.37:', type='run', when='@0.2.1:')
     depends_on('py-click@7.0:', type='run')
     depends_on('py-functools32', when='@:0.2.3^python@:2.99', type='run')
-    depends_on('py-morphio@2.3.4:', type='run')
-    depends_on('py-neurom@1.4.15:', type='run', when='@0.1.14:')
+    depends_on('py-morphio@2.7.0:', type='run')
+    depends_on('py-neurom@1.8.0:', type='run', when='@0.1.14:')
     depends_on('py-pathlib2@2.3.5:', when='@:0.2.3^python@:2.99', type='run')
     depends_on('py-pyyaml', when='@:0.1.14', type='run')
     depends_on('py-numpy@1.14:', type='run')
@@ -35,3 +36,6 @@ class PyMorphTool(PythonPackage):
     depends_on('py-dask@2.19:', when='@0.2.10:', type='run')
     depends_on('py-plotly@4.1:', when='@0.2.10:', type='run')
     depends_on('py-xmltodict@0.12:', when='@0.2.10:', type='run')
+
+    depends_on('py-deprecation@2.1.0:', when='@2.4.0:', type='run')
+    depends_on('py-more-itertools@8.6.0:', when='@2.4.0:', type='run')
