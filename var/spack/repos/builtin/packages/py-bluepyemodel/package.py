28bd6ef5b3c24e5cd965bf6f2acba90dec2bcde9
Alexis Arnaudon <alexis.arnaudon@gmail.com>
2021-05-10 13:45:06 +0200

    use correct class name

--- var/spack/repos/builtin/packages/py-bluepyemodel/package.py
+++ var/spack/repos/builtin/packages/py-bluepyemodel/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class PyDiameterSynthesis(PythonPackage):
+class PyBluePyEmodel(PythonPackage):
     """Python library to optimize and evaluate electrical models."""
 
     homepage = "https://bbpcode.epfl.ch/code/#/admin/projects/cells/BluePyEModel"
