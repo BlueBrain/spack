1d8997a5a80983a59de3b9df5d3fce772246c1aa
Luc Guyot <luc.guyot@epfl.ch>
2021-05-14 16:21:43 +0200

    Apply suggestions from code review
    Co-authored-by: Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>

--- var/spack/repos/builtin/packages/py-atlas-building-tools/package.py
+++ var/spack/repos/builtin/packages/py-atlas-building-tools/package.py
@@ -15,7 +15,7 @@ class PyAtlasBuildingTools(PythonPackage):
 
     depends_on('py-setuptools', type=('build', 'run'))
 
-    depends_on('py-cgal-pybind', type=('build', 'run'))
+    depends_on('py-cgal-pybind@0.1.0:', type=('build', 'run'))
     depends_on('py-click@7.0:', type=('build', 'run'))
     depends_on('py-networkx@2.4:', type=('build', 'run'))
     depends_on('py-nptyping@1.0.1:', type=('build', 'run'))
