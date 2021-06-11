fd9f40371b55364d6d2ab6ecd645f3bf2d6cf788
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2021-06-11 15:55:06 +0000

    Add setuptools for py-webcolors

--- var/spack/repos/builtin/packages/py-webcolors/package.py
+++ var/spack/repos/builtin/packages/py-webcolors/package.py
@@ -15,3 +15,4 @@ class PyWebcolors(PythonPackage):
     version('1.11.1', sha256='76f360636957d1c976db7466bc71dcb713bb95ac8911944dffc55c01cb516de6')
 
     depends_on('python@3.5:', type=('build', 'run'))
+    depends_on('py-setuptools', type=('build'))
