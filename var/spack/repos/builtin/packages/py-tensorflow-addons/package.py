729204aea9679c32bdf5c67a736e93ba2795f7c7
Pierre-Alexandre Fonta <pierre-alexandre.fonta@epfl.ch>
2021-06-21 18:23:48 +0200

    Add py-tensorflow as run dep for py-tensorflow-addons

--- var/spack/repos/builtin/packages/py-tensorflow-addons/package.py
+++ var/spack/repos/builtin/packages/py-tensorflow-addons/package.py
@@ -14,4 +14,5 @@ class PyTensorflowAddons(PythonPackage):
 
     version('0.13.0', sha256='5a8c33ecef5a3daca7f6b27c4b4e3a2badfe55f55a13e97066ee2e03ef98fab0')
 
+    depends_on('py-tensorflow@2.3:2.5', type=('run'))
     depends_on('py-typeguard@2.7:', type=('run'))
