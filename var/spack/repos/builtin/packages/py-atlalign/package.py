08de196638b5d39376b0732ea6024da14d0ef221
Matthias Wolf <matthias.wolf@epfl.ch>
2021-08-09 14:39:46 +0200

    py-atlalign: attempt to patch out lpips.

--- var/spack/repos/builtin/packages/py-atlalign/package.py
+++ var/spack/repos/builtin/packages/py-atlalign/package.py
@@ -30,3 +30,5 @@ class PyAtlalign(PythonPackage):
     # Addons need to be in lockstep with TF
     depends_on('py-tensorflow@2.4', type=('run'))
     depends_on('py-tensorflow-addons@0.12', type=('run'))
+
+    patch('lpips.patch', when='@0.6.0')
