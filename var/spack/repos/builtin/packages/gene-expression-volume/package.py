4dbc02c99ddaaa634008cd0f272e3faa452a5330
lguyot <luc.guyot@epfl.ch>
2021-08-23 16:21:01 +0200

    Deploy nse.gene-expression-volume [BBPP82-482]

---
+++ var/spack/repos/builtin/packages/gene-expression-volume/package.py
@@ -0,0 +1,34 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class GeneExpressionVolume(PythonPackage):
+    """BBP command line interface for the re-alignment of 2D images and 
+    the creation of 3D gene expression volumes. 
+    Used by the atlas building pipeline.
+    """
+    homepage = "https://bbpgitlab.epfl.ch/nse/gene-expression-volume"
+    git      = "git@bbpgitlab.epfl.ch:nse/gene-expression-volume.git"
+
+    version('0.1.0', tag='gene-expression-volume-v0.1.0')
+
+    depends_on('py-setuptools', type=('build', 'run'))
+    depends_on('py-atlalign@0.6.0', type=('build', 'run'))
+    depends_on('py-click@7.0:', type=('build', 'run'))
+    depends_on('py-nptyping@1.0.1:', type=('build', 'run'))
+    depends_on('py-numpy@1.15.0:', type=('build', 'run'))
+    depends_on('py-scikit-image@0.017.2:', type=('build', 'run'))
+    depends_on('py-types-pyaml@5.4.0:', type=('build', 'run'))
+    depends_on('py-voxcell@2.7.4', type=('run', 'build'))
+    depends_on('py-pyaml@5.3.1:', type=('build', 'run'))
+    depends_on('py-pytest', type='test')
+    depends_on('py-mock', type='test')
+
+    @run_after('install')
+    @on_package_attributes(run_tests=True)
+    def test_install(self):
+        python("-m", "pytest", "tests")
