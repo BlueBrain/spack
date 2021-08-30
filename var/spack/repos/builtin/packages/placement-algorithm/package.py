992e91566c54df8da9945f796e4845fe6279b004
aleksei sanin <aleksei.sanin@epfl.ch>
2021-08-30 16:28:02 +0200

    constraint morph-tool in placement-algorithm

--- var/spack/repos/builtin/packages/placement-algorithm/package.py
+++ var/spack/repos/builtin/packages/placement-algorithm/package.py
@@ -29,7 +29,7 @@ class PlacementAlgorithm(PythonPackage):
     depends_on('py-six', type='run')
 
     depends_on('py-morphio@2.0.5:', type='run')
-    depends_on('py-morph-tool@0.1.3:', type='run')
+    depends_on('py-morph-tool@0.1.3:2.8.99', type='run')
     depends_on('py-mpi4py@2.0:', type='run')
     depends_on('py-tqdm@4.0:', type='run')
     depends_on('py-voxcell@2.5:2.6.99', when='@:2.0.99', type='run')
