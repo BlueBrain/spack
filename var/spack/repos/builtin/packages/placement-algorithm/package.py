c1404db8c18f0dcb1b4f89c50a175f5ac8402cea
aleksei sanin <aleksei.sanin@epfl.ch>
2021-08-31 11:18:49 +0200

    try draft of placemenet-algorithm 2.2.0, neuroc, tns

--- var/spack/repos/builtin/packages/placement-algorithm/package.py
+++ var/spack/repos/builtin/packages/placement-algorithm/package.py
@@ -13,6 +13,8 @@ class PlacementAlgorithm(PythonPackage):
     git      = "git@bbpgitlab.epfl.ch:nse/placement-algorithm.git"
 
     version('develop', branch='master')
+    version('2.2.0', branch='clean-deps')
+    version('2.1.4', tag='placement-algorithm-v2.1.4')
     version('2.1.2', tag='placement-algorithm-v2.1.2')
     version('2.1.1', tag='placement-algorithm-v2.1.1')
     version('2.1.0', tag='placement-algorithm-v2.1.0')
@@ -28,13 +30,18 @@ class PlacementAlgorithm(PythonPackage):
     depends_on('py-pandas', type='run')
     depends_on('py-six', type='run')
 
-    depends_on('py-morphio@2.0.5:', type='run')
-    depends_on('py-morph-tool@0.1.3:2.8.99', type='run')
-    depends_on('py-neurom@:2.99', type='run')
+    depends_on('py-morphio@3.0:3.999', type='run', when='@2.2.0:')
+    depends_on('py-morphio@2.0.5:2.99', type='run', when='@:2.1.99')
+    depends_on('py-morph-tool@2.9.0:', type='run', when='@2.2.0:')
+    depends_on('py-morph-tool@0.1.3:2.8.99', type='run', when='@:2.1.99')
+    depends_on('py-neurom@3.0:3.999', type='run', when='@2.2.0:')
+    depends_on('py-neurom@2.0:2.99', type='run', when='@:2.1.99')
+    depends_on('py-neuroc@:0.2.7', type='run', when='@:2.1.99')
+    depends_on('py-neuroc@0.2.8:0.999', type='run', when='@2.2.0:')
     depends_on('py-mpi4py@2.0:', type='run')
     depends_on('py-tqdm@4.0:', type='run')
     depends_on('py-voxcell@2.5:2.6.99', when='@:2.0.99', type='run')
-    depends_on('py-voxcell@2.7:', when='@2.1.0:', type='run')
+    depends_on('py-voxcell@2.7:3.99', when='@2.1.0:', type='run')
     depends_on('py-dask@2.15:', when='@2.0.12:', type='run')
 
-    depends_on('py-region-grower@0.1.5:0.1.99', type='run')
+    depends_on('py-region-grower@0.1.5:0.1.99', type='run', when='@:2.1.99')
--- var/spack/repos/builtin/packages/py-tns/package.py
+++ var/spack/repos/builtin/packages/py-tns/package.py
@@ -14,6 +14,8 @@ class PyTns(PythonPackage):
 
     version('develop', branch='master')
     version('space2', branch='space2')
+    version('2.5.0', tag='TNS-v2.5.0')
+    version('2.4.5', tag='TNS-v2.4.5')
     version('2.4.4', tag='TNS-v2.4.4')
     version('2.4.2', tag='TNS-v2.4.2')
     version('2.3.2', tag='TNS-v2.3.2')
@@ -24,8 +26,10 @@ class PyTns(PythonPackage):
     depends_on('py-setuptools', type='build')
 
     depends_on('py-matplotlib@1.3.1:', type='run')
-    depends_on('py-morphio@2.7.1:', type='run')
-    depends_on('py-neurom@2:2.999', type='run')
+    depends_on('py-morphio@3.0:3.999', type='run', when='@2.5.0:')
+    depends_on('py-morphio@2.7.1:2.99', type='run', when='@:2.4.99')
+    depends_on('py-neurom@3:3.999', type='run', when='@2.5.0:')
+    depends_on('py-neurom@2:2.99', type='run', when='@:2.4.99')
     depends_on('py-numpy@1.15.0:', type='run')
     depends_on('py-scipy@0.13.3:', type='run')
     depends_on('py-tmd@2.0.8:', type='run')
