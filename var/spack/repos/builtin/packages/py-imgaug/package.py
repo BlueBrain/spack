8ebfa405df3d4b8f5e83cd9a2caa25d46093b1c2
lguyot <luc.guyot@epfl.ch>
2021-09-03 11:52:37 +0200

    Removes the --expand=False options from py-atlalign and py-imgaug package.py files: they cause install problems

--- var/spack/repos/builtin/packages/py-imgaug/package.py
+++ var/spack/repos/builtin/packages/py-imgaug/package.py
@@ -13,7 +13,7 @@ class PyImgaug(PythonPackage):
     url = "https://files.pythonhosted.org/packages/4f/96/c55f2985b464ea377238e61026dfb1da54d8edc1534c649e70077e94c642/imgaug-0.2.9.tar.gz"
 
     # The first version below just serves to trigger a rebuild!
-    version('0.2.9.20210902', url=url, sha256='42b0c4c8cbe197d4f5dbd33960a1140f8a0d9c22c0a8851306ecbbc032092de8', expand=False)
+    version('0.2.9.20210902', url=url, sha256='42b0c4c8cbe197d4f5dbd33960a1140f8a0d9c22c0a8851306ecbbc032092de8')
     version('0.2.9', sha256='42b0c4c8cbe197d4f5dbd33960a1140f8a0d9c22c0a8851306ecbbc032092de8')
 
     depends_on('python@3.6:', type=('build', 'run'))
