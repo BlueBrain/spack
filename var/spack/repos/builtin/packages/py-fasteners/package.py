ca4cf5b3c6776db40c2dd0b7e3a1ddece19a516a
arnaudon <alexis.arnaudon@epfl.ch>
2021-09-07 10:59:06 +0200

    update fasterner for bpem

--- var/spack/repos/builtin/packages/py-fasteners/package.py
+++ var/spack/repos/builtin/packages/py-fasteners/package.py
@@ -12,6 +12,8 @@ class PyFasteners(PythonPackage):
     homepage = "https://github.com/harlowja/fasteners"
     url      = "https://pypi.io/packages/source/f/fasteners/fasteners-0.14.1.tar.gz"
 
+
+    version('1.0.4', sha256='221cefad223929e6556515e231f52d8af667579d3c534e1b731ce3d2f923553e')
     version('0.14.1', sha256='427c76773fe036ddfa41e57d89086ea03111bbac57c55fc55f3006d027107e18')
 
     depends_on('py-setuptools',     type='build')
