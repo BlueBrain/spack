e07586a0960b3fad5c86cb6ede2756002c12bab0
alibou <nabil.alibou@epfl.ch>
2021-09-20 18:34:47 +0200

    Deployment of the dke modules : py-nexusforge and py-bba-datafetch [BBPP82-632]: Added bba-datafetch to the module whitelist. Replace the version 1.5.1 of nest-asyncio by the 1.5.0

--- var/spack/repos/builtin/packages/py-nest-asyncio/package.py
+++ var/spack/repos/builtin/packages/py-nest-asyncio/package.py
@@ -8,8 +8,9 @@ class PyNestAsyncio(PythonPackage):
     """Patch asyncio to allow nested event loops."""
 
     homepage = "https://github.com/erdewit/nest_asyncio"
-    url      = "https://pypi.io/packages/source/n/nest-asyncio/nest_asyncio-1.4.0.tar.gz"
+    url = "https://pypi.io/packages/source/n/nest-asyncio/nest_asyncio-1.5.0.tar.gz"
 
+    version('1.5.0', sha256='e6aaf63aeec11ea4eefaa533b89002d0ac45f7896c49cdb1aba5d9dc6559ab74')
     version('1.4.0', sha256='5773054bbc14579b000236f85bc01ecced7ffd045ec8ca4a9809371ec65a59c8')
 
     depends_on('python@3.5:', type=('build', 'run'))
