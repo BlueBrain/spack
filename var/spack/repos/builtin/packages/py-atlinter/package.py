974ec4c02626d26025950e631dd8eb25a3d8ef2a
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-13 14:07:39 +0200

    Add py-atlinter versions up to 0.2.3

--- var/spack/repos/builtin/packages/py-atlinter/package.py
+++ var/spack/repos/builtin/packages/py-atlinter/package.py
@@ -10,12 +10,21 @@ from spack.directives import depends_on, variant, version
 class PyAtlinter(PythonPackage):
     """Interpolation of section images."""
 
-    homepage = "https://bbpgitlab.epfl.ch/project/proj101/atlas_interpolation"
+    homepage = "https://atlas-interpolation.rtfd.io"
     git = "git@bbpgitlab.epfl.ch:project/proj101/atlas_interpolation.git"
 
     maintainers = ["EmilieDel", "Stannislav"]
 
-    version("0.1.0", commit="4339279b40d04716d3385e1c5dbd666359ec3fa1")
+    version(
+        "0.2.3",
+        url="https://files.pythonhosted.org/packages/57/d3/4cdfaacccff677b32687cbcfd689727bc7b3a8f4485401aa3eca157e5aaf/atlinter-0.2.3.tar.gz",
+        sha256="67f102b70bc7eeb450da8af3c19df38610dd081a025f9c04e8f5266d2d0cc6e3",
+    )
+    version("0.2.2", tag="v0.2.2")
+    version("0.2.1", tag="v0.2.1")
+    version("0.2.0", tag="v0.2.0")
+    version("0.1.1", tag="v0.1.1")
+    version("0.1.0", tag="v0.1.0")
 
     variant("cuda", default=False, description="Enable CUDA support")
 
@@ -23,11 +32,17 @@ class PyAtlinter(PythonPackage):
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
 
+    depends_on("mxnet", when="@0.1.1:", type="run")
+    depends_on("py-atlannot", when="@0.2.2:", type="run")
     depends_on("py-atldld@0.2.2", type="run")
     depends_on("py-numpy", type="run")
     depends_on("py-pillow", type="run")
     depends_on("py-pytorch-fid", type="run")
+    depends_on("py-pynrrd", when="@0.2.2:", type="run")
+    depends_on("py-pyyaml", when="@0.1.1:", type="run")
     depends_on("py-requests", type="run")
+    depends_on("py-scikit-image", when="@0.2.2:", type="run")
+    depends_on("py-scipy", when="@0.1.1:", type="run")
     depends_on("py-torch+cuda", when="+cuda", type="run")
     depends_on("py-torch~cuda~cudnn~nccl", when="~cuda", type="run")
     depends_on("py-torchvision", type="run")
