5ee1291fa26db9c86d30d66ef104668f9a49f229
Stanislav Schmidt <stanislav.schmidt@epfl.ch>
2021-10-13 13:56:26 +0200

    Switch to double quotes

--- var/spack/repos/builtin/packages/py-atlinter/package.py
+++ var/spack/repos/builtin/packages/py-atlinter/package.py
@@ -10,24 +10,24 @@ from spack.directives import depends_on, variant, version
 class PyAtlinter(PythonPackage):
     """Interpolation of section images."""
 
-    homepage = 'https://bbpgitlab.epfl.ch/project/proj101/atlas_interpolation'
-    git = 'git@bbpgitlab.epfl.ch:project/proj101/atlas_interpolation.git'
+    homepage = "https://bbpgitlab.epfl.ch/project/proj101/atlas_interpolation"
+    git = "git@bbpgitlab.epfl.ch:project/proj101/atlas_interpolation.git"
 
-    maintainers = ['EmilieDel', 'Stannislav']
+    maintainers = ["EmilieDel", "Stannislav"]
 
-    version('0.1.0', commit='4339279b40d04716d3385e1c5dbd666359ec3fa1')
+    version("0.1.0", commit="4339279b40d04716d3385e1c5dbd666359ec3fa1")
 
-    variant('cuda', default=False, description='Enable CUDA support')
+    variant("cuda", default=False, description="Enable CUDA support")
 
-    depends_on('python@3.7:', type=('build', 'run'))
-    depends_on('py-setuptools', type='build')
-    depends_on('py-setuptools-scm', type='build')
+    depends_on("python@3.7:", type=("build", "run"))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools-scm", type="build")
 
-    depends_on('py-atldld@0.2.2', type='run')
-    depends_on('py-numpy', type='run')
-    depends_on('py-pillow', type='run')
-    depends_on('py-pytorch-fid', type='run')
-    depends_on('py-requests', type='run')
-    depends_on('py-torch+cuda', when='+cuda', type='run')
-    depends_on('py-torch~cuda~cudnn~nccl', when='~cuda', type='run')
-    depends_on('py-torchvision', type='run')
+    depends_on("py-atldld@0.2.2", type="run")
+    depends_on("py-numpy", type="run")
+    depends_on("py-pillow", type="run")
+    depends_on("py-pytorch-fid", type="run")
+    depends_on("py-requests", type="run")
+    depends_on("py-torch+cuda", when="+cuda", type="run")
+    depends_on("py-torch~cuda~cudnn~nccl", when="~cuda", type="run")
+    depends_on("py-torchvision", type="run")
