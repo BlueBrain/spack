5d6f06946f476b86c155194615ddda26016dbc6f
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2022-01-13 17:04:18 +0100

    Update compilers.yaml and deployment script

--- sysconfig/jusuf/deploy_simstack.sh
+++ sysconfig/jusuf/deploy_simstack.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 module --force purge
 
-module load Intel/2019.5.281-GCC-8.3.0 IntelMPI/2019.6.154
+module load Stages/2022
+module load Intel/2021.4.0 ParaStationMPI/5.5.0-1
+module load HDF5/1.12.1
 
 set -e
 # Deployment directory
 #date=$(date '+%d-%m-%Y')
-date='10-08-2020'
-DEPLOYMENT_HOME=/p/project/cvsk25/software-deployment/HBP/jusuf/$date
+project='icei-hbp-2020-0013'
+#date='03-06-2021'
+date='10-01-2022'
+DEPLOYMENT_HOME=/p/project/$project/software-deployment/HBP/jusuf/$date
 mkdir -p $DEPLOYMENT_HOME/sources
 
 # Clone spack repository and setup environment
 cd $DEPLOYMENT_HOME/sources
-[[ -d spack ]] || git clone https://github.com/BlueBrain/spack.git
+[[ -d spack ]] || git clone https://github.com/BlueBrain/spack.git -b jusuf_deployment_2022
 
 # Setup environment
 export SPACK_ROOT=`pwd`/spack
@@ -25,43 +29,45 @@ cp $SPACK_ROOT/sysconfig/jusuf/* $SPACK_ROOT/etc/spack/defaults/linux/
 
 # Directory for deployment
 export SPACK_INSTALL_PREFIX=$DEPLOYMENT_HOME
-export HOME=/p/project/cvsk25/blancoalonso1/
+export HOME=/p/home/jusers/blancoalonso1/jusuf
 module list
 
-#spack mirror add local_filesystem /p/project/cvsk25/blancoalonso1/jusuf/mirrors
 spack mirror list
-
-# Python 2 packages
-#spack spec -Il neuron %intel ^python@2.7.15
-#spack install --dirty --keep-stage -v neuron %intel ^python@2.7.15
-
-#spack spec -Il neuron~mpi %intel ^python@2.7.15
-#spack install --dirty --keep-stage -v neuron~mpi %intel ^python@2.7.15
+#spack mirror rm local_filesystem
+#spack mirror add local_filesystem /p/project/$project/software-deployment/HBP/jusuf/$date/mirrors
+#spack mirror add local_filesystem /p/project/cvsk25/blancoalonso1/jusuf/mirrors
 
 # Python 3 packages
-module load Python/3.6.8
-module load SciPy-Stack/2019a-Python-3.6.8
+module load Python/3.9.6
+module load SciPy-Stack/2021b
 module list
+export LC_ALL=en_US.utf8
+export LANG=en_US.utf8
+export LC_CTYPE=en_US.UTF-8
 
-neurodamus_deps="^coreneuron ^python@3.6.8"
+PYTHON_VERSION='^python@3.9.6'
+neurodamus_deps="^coreneuron $PYTHON_VERSION"
 spack spec -Il neurodamus-hippocampus+coreneuron %intel $neurodamus_deps
 for nd in neurodamus-hippocampus neurodamus-neocortex neurodamus-mousify
 do
    spack install --keep-stage --dirty -v $nd+coreneuron %intel $neurodamus_deps
 done
 
-spack spec -Il neuron~mpi %intel ^python@3.6.8
+#spack spec -Il py-neurodamus%intel $PYTHON_VERSION
+#spack install --dirty --keep-stage py-neurodamus%intel $PYTHON_VERSION
+
+#spack spec -Il neuron~mpi %intel ^python@3.6.8
 #spack install --dirty --keep-stage -v neuron~mpi %intel ^python@3.6.8
 
-spack spec -Il py-bluepy%gcc ^python@3.6.8
-#spack install --dirty --keep-stage -v py-bluepy%gcc ^python@3.6.8
+#spack spec -Il py-bluepy%gcc $PYTHON_VERSION
+#spack install --dirty --keep-stage -v py-bluepy%gcc $PYTHON_VERSION
 
-spack spec -Il py-sonata-network-reduction%gcc ^python@3.6.8 ^zeromq%intel
-#spack install --dirty --keep-stage -v py-sonata-network-reduction%gcc ^python@3.6.8 ^zeromq%intel
+#spack spec -Il py-sonata-network-reduction%gcc $PYTHON_VERSION ^libzmq%intel
+#spack install --dirty --keep-stage -v py-sonata-network-reduction%gcc $PYTHON_VERSION ^libzmq%intel
 
-spack spec -Il py-bluepyopt%gcc ^python@3.6.8 ^zeromq%intel
-#spack install --keep-stage --dirty -v py-bluepyopt%gcc ^python@3.6.8 ^zeromq%intel
+#spack spec -Il py-bluepyopt%gcc $PYTHON_VERSION ^libzmq%intel
+#spack install --keep-stage --dirty -v py-bluepyopt%gcc $PYTHON_VERSION ^libzmq%intel
 
-spack module tcl refresh --delete-tree -y
-cd $DEPLOYMENT_HOME/modules/tcl/linux-centos7-haswell
+spack module tcl refresh --delete-tree --latest -y
+cd $DEPLOYMENT_HOME/modules/tcl/linux-centos7-zen2
 find py* -type f -print0|xargs -0 sed -i '/PYTHONPATH.*\/neuron-/d'
