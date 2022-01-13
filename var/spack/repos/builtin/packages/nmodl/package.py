997df75457251e878557a6da35d30474c98cae5c
Ioannis Magkanaris <iomagkanaris@gmail.com>
2022-01-13 13:59:20 +0100

    Added fix in nmodl for threshdetect

--- var/spack/repos/builtin/packages/nmodl/package.py
+++ var/spack/repos/builtin/packages/nmodl/package.py
@@ -15,6 +15,7 @@ class Nmodl(CMakePackage):
 
     version('develop', branch='master', submodules=True)
     # 0.3.0 > 0.3b > 0.3 as far as Spack is concerned
+    version('0.3.0.20220110', commit='3cd1479') # fix for threshdetect
     version('0.3.0.20211001', commit='d28f0bee20bc55ccda9ba9371f2118127138100d') # this is on master
     version('0.3.0.20210921', commit='746aed7f3ee8849dfff6423bb5d7b93b6aa07c0e') # pramodk/net_init_acc_fix branch
     version('0.3.0.20210917', commit='1845c2746faaedaad533de069417bdce084a1bac')
