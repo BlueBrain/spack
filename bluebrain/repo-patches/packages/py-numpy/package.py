a55ceb1db7a6bd807fc583dd16cf8d3aa08694e4
Olli Lupton <oliver.lupton@epfl.ch>
2022-01-17 10:44:46 +0100

    Improve docs.

--- bluebrain/repo-patches/packages/py-numpy/package.py
+++ bluebrain/repo-patches/packages/py-numpy/package.py
@@ -3,6 +3,10 @@ from spack.pkg.builtin.py_numpy import PyNumpy as BuiltinPyNumpy
 
 class PyNumpy(BuiltinPyNumpy):
     __doc__ = BuiltinPyNumpy.__doc__
+    # With the CPPFLAGS/CFLAGS/CXXFLAGS fix below then there does not appear
+    # to be any compiler error message with NVHPC, but at least with 21.11
+    # then an llc process gets stuck and eventually killed when compiling
+    # loops.c
     conflicts('%nvhpc')
 
     def setup_build_environment(self, env):
