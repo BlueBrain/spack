de41668e163b9cc2e734aa20cae4eb79caf24beb
Matthias Wolf <matthias.wolf@epfl.ch>
2022-01-18 14:46:55 +0100

    py-rtree: propagate env

---
+++ bluebrain/repo-patches/packages/py-rtree/package.py
@@ -0,0 +1,9 @@
+from spack import *
+from spack.pkg.builtin.py_rtree import PyRtree as BuiltinPyRtree
+
+
+class PyRtree(BuiltinPyRtree):
+    __doc__ = BuiltinPyRtree.__doc__
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        self.setup_run_environment(env)
