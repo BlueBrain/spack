b1767cf6dd9723b2cd3b1742da784af3f2c7742f
Matthias Wolf <matthias.wolf@epfl.ch>
2022-02-14 11:14:05 +0100

    appease the snowflake

--- bluebrain/repo-bluebrain/packages/sim-model/package.py
+++ bluebrain/repo-bluebrain/packages/sim-model/package.py
@@ -3,11 +3,11 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-from spack import *
-
-from contextlib import contextmanager
 import os
 import shutil
+from contextlib import contextmanager
+
+from spack import *
 
 
 class SimModel(Package):
