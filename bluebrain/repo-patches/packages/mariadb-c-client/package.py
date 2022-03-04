c230d7c74d886ced0b5f90ed4d4b909c2697b26a
Omar Awile <omar.awile@gmail.com>
2022-03-04 16:22:49 +0100

    Update bluebrain/repo-patches/packages/mariadb-c-client/package.py
    Co-authored-by: Matthias Wolf <m+git@sushinara.net>

--- bluebrain/repo-patches/packages/mariadb-c-client/package.py
+++ bluebrain/repo-patches/packages/mariadb-c-client/package.py
@@ -5,6 +5,5 @@ from spack.pkg.builtin.mariadb_c_client import MariadbCClient as BuiltinMariadbC
 class MariadbCClient(BuiltinMariadbCClient):
     __doc__ = BuiltinMariadbCClient.__doc__
 
-
     def setup_dependent_run_environment(self, env, dependent_spec):
         env.prepend_path("LD_LIBRARY_PATH", join_path(self.prefix, "lib/mariadb"))
