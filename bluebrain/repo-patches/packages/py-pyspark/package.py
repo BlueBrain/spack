af865cfb9235ecd7a5e241310e6f6655492eef17
Matthias Wolf <matthias.wolf@epfl.ch>
2022-03-08 15:08:29 +0100

    py-py-py-pyspark: fix the deps

--- bluebrain/repo-patches/packages/py-pyspark/package.py
+++ bluebrain/repo-patches/packages/py-pyspark/package.py
@@ -9,6 +9,8 @@ class PyPyspark(BuiltinPyPyspark):
     version('3.1.3', sha256='39ac641ef5559a3d1286154779fc990316e9934520853615ae4785c1af52d14b')
     version('3.1.2', sha256='5e25ebb18756e9715f4d26848cc7e558035025da74b4fc325a0ebc05ff538e65')
 
+    depends_on('py-py4j@0.10.9', when='@3.1.2', type=('build', 'run'))
+    depends_on('py-py4j@0.10.9', when='@3.1.3', type=('build', 'run'))
     depends_on('py-py4j@0.10.9.3', when='@3.2.1', type=('build', 'run'))
 
     def setup_run_environment(self, env):
