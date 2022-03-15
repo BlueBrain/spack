661108a1b9144249c9215ef6634bd6fe748b0023
Gianluca Ficarelli <gianluca.ficarelli@epfl.ch>
2022-03-15 18:26:34 +0100

    Bump gene-expression-volume and dependencies
    Dependencies:
    
    - py-atlalign
    - py-atldld
    - py-tensorflow (from upstream)
    - py-tensorflow-addons
    - py-libclang (from upstream)
    - py-tensorboard-data-server (from upstream)
    - py-tensorboard-plugin-wit (from upstream)
    - py-tensorboard (from upstream)
    - py-tensorflow-estimator (from upstream)

--- bluebrain/repo-bluebrain/packages/gene-expression-volume/package.py
+++ bluebrain/repo-bluebrain/packages/gene-expression-volume/package.py
@@ -14,23 +14,21 @@ class GeneExpressionVolume(PythonPackage):
     homepage = "https://bbpgitlab.epfl.ch/nse/gene-expression-volume"
     git      = "git@bbpgitlab.epfl.ch:nse/gene-expression-volume.git"
 
-    version('0.1.0', tag='gene-expression-volume-v0.1.0')
+    version('0.1.1', tag='gene-expression-volume-v0.1.1')
 
     depends_on('py-setuptools', type=('build', 'run'))
-    depends_on('py-atlalign@0.6.0', type=('build', 'run'))
+    depends_on('py-atlalign@0.6.1:', type=('build', 'run'))
+    depends_on('py-atldld@0.3.3:', type=('build', 'run'))
     depends_on('py-click@7.0:', type=('build', 'run'))
     depends_on('py-nptyping@1.0.1:', type=('build', 'run'))
     depends_on('py-numpy@1.15.0:', type=('build', 'run'))
+    depends_on('py-pyyaml@5.3.1:', type=('build', 'run'))
     depends_on('py-scikit-image@0.17.2:', type=('build', 'run'))
     depends_on('py-types-pyyaml@5.4.0:', type=('build', 'run'))
-    depends_on('py-voxcell@2.7.4:2.999', type=('run', 'build'))
-    depends_on('py-pyyaml@5.3.1:', type=('build', 'run'))
+    depends_on('py-voxcell@2.7.4:', type=('run', 'build'))
+
     depends_on('py-pytest', type='test')
     depends_on('py-mock', type='test')
-
-    def patch(self):
-        filter_file('"lpips_tf.+egg=lpips_tf",', '', 'setup.py')
-        filter_file('"tensorflow==[0-9.]+",', '', 'setup.py')
 
     @run_after('install')
     @on_package_attributes(run_tests=True)
--- bluebrain/repo-bluebrain/packages/py-atldld/package.py
+++ bluebrain/repo-bluebrain/packages/py-atldld/package.py
@@ -11,14 +11,15 @@ class PyAtldld(PythonPackage):
     """Search, download, and prepare brain atlas data."""
 
     homepage = "atlas-download-tools.rtfd.io"
-    git = "https://github.com/BlueBrain/Atlas-Download-Tools.git"
+    pypi = "atldld/atldld-0.3.3.tar.gz"
 
     maintainers = ["EmilieDel", "jankrepl", "Stannislav"]
 
-    version("0.3.2", tag="v0.3.2")
-    version("0.3.1", tag="v0.3.1")
-    version("0.3.0", tag="v0.3.0")
-    version("0.2.2", tag="v0.2.2")
+    version('0.3.3', sha256='c50dfabed5f318f15fdddc031d48e0cfd2b15846d307fbc57c797fa35edfef40')
+    version('0.3.2', sha256='e39392ebd2ffb675b5b98016f5982da6cbcca0249622e0e6509c98c6f07ee3bc')
+    version('0.3.1', sha256='5b4b724392378771baa14cf15058f1f8ab33c587ff7a9f99e3278135dfb7ff64')
+    version('0.3.0', sha256='78186f994262505ebae62ad78858f0ff4ec95a1c7e5aa9935871c2485aa4d947')
+    version('0.2.2', sha256='4bdbb9ccc8e164c970940fc729a10bf883a67035e8c636261913cecb351835d3')
 
     # Build dependencies
     depends_on("python@3.7:", type=("build", "run"))
@@ -31,8 +32,8 @@ class PyAtldld(PythonPackage):
     depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-opencv-python", type=("build", "run"))
-    depends_on("py-pandas", type=("build", "run"))
+    depends_on("py-pandas", when="@:0.3.2", type=("build", "run"))
     depends_on("py-pillow", when="@0.3.1:", type=("build", "run"))
-    depends_on("py-requests", type=("build", "run"))
+    depends_on("py-requests", when="@:0.3.2", type=("build", "run"))
     depends_on("py-responses", type=("build", "run"))
     depends_on("py-scikit-image", type=("build", "run"))
