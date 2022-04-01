209051edc2caef8e120ed14041859c7c61586246
Nabil-AL <63203348+Nabil-AL@users.noreply.github.com>
2022-04-01 15:16:12 +0200

    Update bluebrain/repo-patches/packages/py-elasticsearch/package.py
    Co-authored-by: Matthias Wolf <m+git@sushinara.net>

--- bluebrain/repo-patches/packages/py-elasticsearch/package.py
+++ bluebrain/repo-patches/packages/py-elasticsearch/package.py
@@ -11,6 +11,3 @@ class PyElasticSearch(BuiltinPyElasticSearch):
     __doc__ = BuiltinPyElasticSearch.__doc__
 
     version('7.6.0', sha256='d228b2d37ac0865f7631335268172dbdaa426adec1da3ed006dddf05134f89c8')
-
-    depends_on('py-setuptools', type='build')
-    depends_on('py-urllib3@1.8:1', type=('build', 'run'))
