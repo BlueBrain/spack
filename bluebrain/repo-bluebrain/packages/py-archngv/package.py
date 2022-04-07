e1b10a23d99fc7bd1fb4ef71c32aaa75fe6ca8ab
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2022-04-07 13:32:03 +0200

    Remove uneeded variant

--- bluebrain/repo-bluebrain/packages/py-archngv/package.py
+++ bluebrain/repo-bluebrain/packages/py-archngv/package.py
@@ -15,8 +15,6 @@ class PyArchngv(PythonPackage):
     version("develop", branch="main")
     version("2.0.0", tag="ArchNGV-v2.0.0")
 
-    variant("all", default=True, description="Circuit building support.")
-
     depends_on("py-setuptools@42:", type="build")
 
     depends_on("py-numpy@1.19.5:", type="run")
