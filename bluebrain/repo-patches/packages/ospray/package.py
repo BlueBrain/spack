1a189e926cf97013d6550096d4b724f0cec1eb36
Nadir Román Guerrero <NadirRoGue@users.noreply.github.com>
2022-04-20 13:27:51 +0200

    Typo fix on OSPRay package

--- bluebrain/repo-patches/packages/ospray/package.py
+++ bluebrain/repo-patches/packages/ospray/package.py
@@ -36,7 +36,7 @@ class Ospray(CMakePackage):
             "-DOSPRAY_ENABLE_TUTORIALS=OFF",
             "-DOSPRAY_MODULE_MPI=ON"
         ]
-        if self.spec.satisifes('@:1.8.5'):
+        if self.spec.satisfies('@:1.8.5'):
             args.append("-DOSPRAY_ENABLE_APPS:BOOL={0}".format(
                         "ON" if "+apps" in self.spec else "OFF"))
         elif self.spec.satisfies('@:2.9.0'):
