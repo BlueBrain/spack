9d83ebb6723a81fc1d48ac660aef76ea9336ecdb
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2022-04-27 13:23:05 +0300

    Change only the common tag

--- bluebrain/repo-bluebrain/packages/neurodamus-neocortex/package.py
+++ bluebrain/repo-bluebrain/packages/neurodamus-neocortex/package.py
@@ -9,7 +9,6 @@ from .neurodamus_model import (
     NeurodamusModel,
     version_from_model_core_dep,
     version_from_model_ndpy_dep,
-    debug_version
 )
 
 
@@ -27,7 +26,8 @@ class NeurodamusNeocortex(NeurodamusModel):
     version('develop', branch='main', submodules=True, get_full_repo=True)
     # IMPORTANT: Register new versions only using version_from_model_*
     # Final version name is combined e.g. "1.0-3.0.1"
-    debug_version('1.6.1')
+    version_from_model_ndpy_dep('1.6')
+    version_from_model_core_dep('1.5', '3.3.4')
 
     @run_before('build_model')
     def prepare_mods(self):
