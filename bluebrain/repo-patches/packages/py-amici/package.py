cbec2eaaabf511932d7a00dddf659dca6cb027d9
Matthias Wolf <matthias.wolf@epfl.ch>
2022-05-12 20:36:34 +0200

    py-amici: new soft ware
    Fixes BSD-276.

---
+++ bluebrain/repo-patches/packages/py-amici/package.py
@@ -0,0 +1,28 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyAmici(PythonPackage):
+    """Advanced Multilanguage Interface for CVODES and IDAS"""
+
+    homepage = "https://github.com/AMICI-dev/AMICI"
+    pypi     = "amici/amici-0.11.28.tar.gz"
+
+    version('0.11.28', sha256='a8ddda70d8ebdc40600b4ad2ea02eb26e765ca0e594b957f61866b8c84255d5b')
+
+    depends_on('cmake', type='build')
+
+    depends_on('blas')
+    depends_on('boost')
+    depends_on('hdf5')
+    depends_on('swig')
+
+    depends_on('sbml+python', type='run')
+    depends_on('py-toposort', type='run')
+
+    def setup_build_environment(self, env):
+        env.set("BLAS_LIBS", " ".join(self.spec['blas'].libs))
---
+++ bluebrain/repo-patches/packages/sbml/package.py
@@ -0,0 +1,17 @@
+import os
+from spack import *
+from spack.pkg.builtin.sbml import Sbml as BuiltinSbml
+
+
+class Sbml(BuiltinSbml):
+    __doc__ = BuiltinSbml.__doc__
+
+    extends('python', when='+python')
+
+    def setup_run_environment(self, env):
+        ppath = self.spec['python'].package.site_packages_dir
+        if os.path.isdir(self.prefix.lib64):
+            ppath = ppath.replace('lib', 'lib64')
+        # This package nests one level deeper than expected via a .pth file
+        ppath = os.path.join(self.prefix, ppath, 'libsbml')
+        env.prepend_path('PYTHONPATH', ppath)
