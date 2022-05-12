8c6dfde09c9122c2f4941c15f89b12a68ea55237
Matthias Wolf <matthias.wolf@epfl.ch>
2022-05-12 22:35:11 +0200

    Fix wurlitzer

--- bluebrain/repo-patches/packages/py-wurlitzer/package.py
+++ bluebrain/repo-patches/packages/py-wurlitzer/package.py
@@ -13,3 +13,5 @@ class PyWurlitzer(PythonPackage):
     pypi     = "wurlitzer/wurlitzer-3.0.2.tar.gz"
 
     version('3.0.2', sha256='36051ac530ddb461a86b6227c4b09d95f30a1d1043de2b4a592e97ae8a84fcdf')
+
+    depends_on('py-setuptools', type='build')
