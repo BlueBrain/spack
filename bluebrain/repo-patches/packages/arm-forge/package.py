1c286e359eef6b0c510646ac2285b955bbe9d762
Sergio <sergio.rivasgomez@epfl.ch>
2022-05-16 10:28:38 +0200

    Bumps arm-forge to v22.0.1

--- bluebrain/repo-patches/packages/arm-forge/package.py
+++ bluebrain/repo-patches/packages/arm-forge/package.py
@@ -6,9 +6,9 @@ class ArmForge(BuiltinArmForge):
     __doc__ = BuiltinArmForge.__doc__
 
     version(
-        "21.1.2-Linux-x86_64",
-        sha256="ebc99fa3461d2cd968e4d304c11b70cc8d9c5a2acd68681cec2067c128255cd5",
-        url="https://content.allinea.com/downloads/arm-forge-21.1.2-linux-x86_64.tar",
+        "22.0.1-Linux-x86_64",
+        sha256="8f8a61c159665d3de3bc5334ed97bdb4966bfbdb91b65d32d162d489eb2219ac",
+        url="https://content.allinea.com/downloads/arm-forge-22.0.1-linux-x86_64.tar",
     )
 
     version(
