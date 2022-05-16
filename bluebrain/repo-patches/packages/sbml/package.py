c4d26cf75829c6a1dfb290b550ae30603a2e5e4b
Matthias Wolf <matthias.wolf@epfl.ch>
2022-05-16 10:35:51 +0200

    sbml: fix Python install

--- bluebrain/repo-patches/packages/sbml/package.py
+++ bluebrain/repo-patches/packages/sbml/package.py
@@ -1,4 +1,5 @@
 import os
+import shlex
 
 from spack import *
 from spack.pkg.builtin.sbml import Sbml as BuiltinSbml
@@ -7,19 +8,28 @@ from spack.pkg.builtin.sbml import Sbml as BuiltinSbml
 class Sbml(BuiltinSbml):
     __doc__ = BuiltinSbml.__doc__
 
-    depends_on('py-setuptools', when='+python')
+    depends_on('py-setuptools', type='build', when='+python')
     extends('python', when='+python')
 
     def cmake_args(self):
         args = super().cmake_args()
-        if '+python' in spec:
-            args.append(
-                '-DPYTHON_INSTALL_WITH_SETUP:BOOL=ON'
-            )
+        if '+python' in self.spec:
+            args.extend([
+                '-DPYTHON_INSTALL_IN_PREFIX:BOOL=OFF',
+                '-DPYTHON_INSTALL_WITH_SETUP:BOOL=ON',
+            ])
+        return args
 
-    def setup_run_environment(self, env):
-        ppath = self.spec['python'].package.site_packages_dir
-        if os.path.isdir(self.prefix.lib64):
-            ppath = ppath.replace('lib', 'lib64')
-        ppath = os.path.join(self.prefix, ppath)
-        env.prepend_path('PYTHONPATH', ppath)
+    def patch(self):
+        if self.spec.satisfies('+python'):
+            args = shlex.join(
+                self.spec['py-setuptools'].package.install_args(
+                    self.spec,
+                    self.prefix
+                )
+            )
+            filter_file(
+                'setup.py install',
+                'setup.py install {0}'.format(args),
+                'src/bindings/python/CMakeLists.txt'
+            )
