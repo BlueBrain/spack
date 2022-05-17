2454a334d5bdad136bb2c7b0241dc3f68b887332
Matthias Wolf <matthias.wolf@epfl.ch>
2022-05-17 13:25:01 +0200

    Even moar run dependencies

--- bluebrain/repo-patches/packages/py-amici/package.py
+++ bluebrain/repo-patches/packages/py-amici/package.py
@@ -14,9 +14,8 @@ class PyAmici(PythonPackage):
 
     version('0.11.28', sha256='a8ddda70d8ebdc40600b4ad2ea02eb26e765ca0e594b957f61866b8c84255d5b')
 
-    depends_on('cmake', type='build')
-
-    depends_on('blas')
+    depends_on('cmake', type=('build', 'run'))
+    depends_on('blas', type=('build', 'run'))
     depends_on('boost')
     depends_on('hdf5+cxx+hl', type=('build', 'run'))
     depends_on('swig', type=('build', 'run'))
@@ -29,6 +28,9 @@ class PyAmici(PythonPackage):
     depends_on('py-sympy@1.9:', type=('build', 'run'))
     depends_on('py-toposort', type=('build', 'run'))
     depends_on('py-wurlitzer', type=('build', 'run'))
+
+    def setup_run_environment(self, env):
+        env.set("BLAS_LIBS", " ".join(self.spec['blas'].libs))
 
     def setup_build_environment(self, env):
         env.set("BLAS_LIBS", " ".join(self.spec['blas'].libs))
