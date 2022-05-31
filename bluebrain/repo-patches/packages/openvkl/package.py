09c40b482189109b5c6f8d51a2f10c813ffc06cd
Matthias Wolf <matthias.wolf@epfl.ch>
2022-05-31 10:51:06 +0200

    containerize: clean packages
    Fix stuff that keeps spack containerize from working.

--- bluebrain/repo-patches/packages/openvkl/package.py
+++ bluebrain/repo-patches/packages/openvkl/package.py
@@ -8,7 +8,7 @@ from spack import *
 
 class Openvkl(CMakePackage):
     """
-    Intel® Open Volume Kernel Library (Intel Open VKL) is a
+    Intel Open Volume Kernel Library (Intel Open VKL) is a
     collection of high-performance volume computation kernels,
     developed at Intel.
     """
