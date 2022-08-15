8ba66ebec2fb12fd0f7120a8c1e026e54afd76dd
Matthias Wolf <matthias.wolf@epfl.ch>
2022-08-15 15:35:45 +0200

    py-humanfriendly: bump

---
+++ bluebrain/repo-patches/packages/py-humanfriendly/package.py
@@ -0,0 +1,8 @@
+from spack import *
+from spack.pkg.builtin.py_humanfriendly import PyHumanfriendly as BuiltinPyHumanfriendly
+
+
+class PyHumanfriendly(BuiltinPyHumanfriendly):
+    __doc__ = BuiltinPyHumanfriendly.__doc__
+
+    version('9.1', sha256='066562956639ab21ff2676d1fda0b5987e985c534fc76700a19bd54bcb81121d')
