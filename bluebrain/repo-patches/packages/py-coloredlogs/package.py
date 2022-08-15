d886e8be452808015109dc9b56497cf08690c9cb
Matthias Wolf <matthias.wolf@epfl.ch>
2022-08-15 14:18:24 +0200

    py-coloredlogs: add 15.0

---
+++ bluebrain/repo-patches/packages/py-coloredlogs/package.py
@@ -0,0 +1,8 @@
+from spack import *
+from spack.pkg.builtin.py_coloredlogs import PyColoredlogs as BuiltinPyColoredlogs
+
+
+class PyColoredlogs(BuiltinPyColoredlogs):
+    __doc__ = BuiltinPyColoredlogs.__doc__
+
+    version('15.0', sha256='5e78691e2673a8e294499e1832bb13efcfb44a86b92e18109fa18951093218ab')
