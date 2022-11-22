111f92e8a5130059b63cc2c556dcc7b20290bd1a
Katta <alessandro.cattabiani@epfl.ch>
2022-11-22 13:20:52 +0100

    DO NOT MERGE
    Just to check changes

--- bluebrain/repo-bluebrain/packages/neurodamus-model/package.py
+++ bluebrain/repo-bluebrain/packages/neurodamus-model/package.py
@@ -54,7 +54,8 @@ class NeurodamusModel(SimModel):
     resource(
         name='common_mods',
         git='ssh://git@bbpgitlab.epfl.ch/hpc/sim/models/common.git',
-        tag='2.6.3',
+        branch='katta/tweak_vasccouplingB'
+    #    tag='2.6.3',
         destination='common_latest'
     )
 
