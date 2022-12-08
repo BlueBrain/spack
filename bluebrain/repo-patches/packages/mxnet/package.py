01df8c0fcd33eeac4820a869013c59535925fea0
Erik Heeren <erik.heeren@epfl.ch>
2022-12-08 11:01:22 +0100

    mxnet: define CMAKE_INSTALL_INCLUDEDIR
    Add some comments to explain the patch as well

--- bluebrain/repo-patches/packages/mxnet/package.py
+++ bluebrain/repo-patches/packages/mxnet/package.py
@@ -15,4 +15,16 @@ class Mxnet(BuiltinMxnet):
 
     def setup_build_environment(self, env):
         if "intel-onapi-mkl" in self.spec:
+            # Without this, the build can't find MKL
+            # Without MKL, it tries to use OpenBLAS
+            # We insist on intel-oneapi-mkl as BLAS provider in packages.yaml
             env.set("MKL_ROOT", os.path.dirname(self.spec["intel-oneapi-mkl"].headers[0]))
+
+    def cmake_args(self):
+        args = super().cmake_args()
+
+        if 'intel-oneapi-mkl' in self.spec:
+            # If we don't set it explicitly, it is blank for some reason,
+            # resulting in a failed attempt to install mkldnn to /mkldnn
+            args.append(self.define('CMAKE_INSTALL_INCLUDEDIR', 'include'))
+        return args
