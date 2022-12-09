5fae5d99222536a758a8657fcfabbecd28ed328e
Erik Heeren <erik.heeren@epfl.ch>
2022-12-09 10:40:59 +0100

    node-js: deprecate 15.3.0

--- bluebrain/repo-patches/packages/node-js/package.py
+++ bluebrain/repo-patches/packages/node-js/package.py
@@ -10,3 +10,9 @@ class NodeJs(BuiltinNodeJs):
         sha256="ba8174dda00d5b90943f37c6a180a1d37c861d91e04a4cb38dc1c0c74981c186",
         preferred=True
     )
+    # Explicitly deprecate, because stage-externals keeps trying to build 15.3.0
+    version(
+        "15.3.0",
+        sha256="cadfa384a5f14591b84ce07a1afe529f28deb0d43366fb0ae4e78afba96bfaf2",
+        deprecated=True
+    )
