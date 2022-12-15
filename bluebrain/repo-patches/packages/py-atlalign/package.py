a6f7ef9b05d044fc14a88b2b509b2ab3c06c1a61
Erik Heeren <erik.heeren@epfl.ch>
2022-12-15 11:23:22 +0100

    py-atlalign: disable py-tensorflow-addons dependency for now

--- bluebrain/repo-patches/packages/py-atlalign/package.py
+++ bluebrain/repo-patches/packages/py-atlalign/package.py
@@ -31,7 +31,7 @@ class PyAtlalign(PythonPackage):
     depends_on('py-scipy', type=('run'))
     # Addons need to be in lockstep with TF
     depends_on('py-tensorflow@2.4:', type=('run'))
-    depends_on('py-tensorflow-addons', type=('run'))
+    #depends_on('py-tensorflow-addons', type=('run'))
 
     patch('lpips.patch', when='@0.6.0')
 
