6ac2d6c340dd23e5127cdff99f76484a6ab22088
Erik Heeren <erik.heeren@epfl.ch>
2023-02-10 10:38:24 +0100

    nopackage: formatting validate-commit-message.py
    Placeholders for new functions, and first type hints

--- .github/workflows/validate-commit-message.py
+++ .github/workflows/validate-commit-message.py
@@ -8,74 +8,99 @@ from argparse import ArgumentParser
 
 from git import Repo
 
-KEYWORDS = ['nopackage', 'deploy', 'docs']
+KEYWORDS = ["nopackage", "deploy", "docs"]
 EXISTING_PACKAGES = []
 
 
-def prefix_invalid(prefix):
-    package = prefix.split(':')[0]
+def prefix_invalid(line: str) -> bool:
+    """
+    Split the given line on a colon to look for the prefix, and check whether it is valid
+    """
+    package = line.split(":")[0]
     if package.strip() not in EXISTING_PACKAGES and package not in KEYWORDS:
         return True
 
     return False
 
 
-def main(title):
-    repo = Repo('.')
+def get_changed_packages() -> list:
+    """
+    Return a list of package names which changed in the PR
+    """
+
+
+def docs_changed() -> bool:
+    """
+    Check whether anything changed in docs
+    """
+
+
+def deploy_changed() -> bool:
+    """
+    Check whether anything related to deploy changed
+    """
+
+
+def main(title: str):
+    repo = Repo(".")
 
     faulty_commits = []
 
     if prefix_invalid(title):
-        msg = textwrap.dedent(f"""\
+        msg = textwrap.dedent(
+            f"""\
             * Pull Request Title
               > {title}
 
               Pull request title needs to be compliant as well, '
               as it will be used for the merge/squash commit'
-            """)
+            """
+        )
         faulty_commits.append(msg)
 
     for commit in repo.iter_commits():
-        print(f'Commit: {commit.message} (parents: {commit.parents})')
+        print(f"Commit: {commit.message} (parents: {commit.parents})")
         if len(commit.parents) > 1:
-            print('Not going beyond a merge commit')
+            print("Not going beyond a merge commit")
             break
 
         prefix = commit.message.splitlines()[0]
         if prefix_invalid(prefix):
             quoted_commit_message = textwrap.indent(commit.message, prefix="  > ")
-            msg = f'* {commit.hexsha}\n{quoted_commit_message}'
+            msg = f"* {commit.hexsha}\n{quoted_commit_message}"
             faulty_commits.append(msg)
 
     if faulty_commits:
-        warning = 'These commits are not formatted correctly. '
-        warning += 'Please amend them to start with one of:\n'
-        warning += '* \\<package>: \n'
+        warning = "These commits are not formatted correctly. "
+        warning += "Please amend them to start with one of:\n"
+        warning += "* \\<package>: \n"
         warning += f'* {", ".join(keyword + ":" for keyword in KEYWORDS)}\n\n'
         warning += "### Faulty commits:\n"
         faulty_commits.insert(0, warning)
-        with open('faulty_commits.txt', 'w') as fp:
-            fp.write('\n'.join(faulty_commits))
-        with open(os.environ['GITHUB_OUTPUT'], 'a') as fp:
+        with open("faulty_commits.txt", "w") as fp:
+            fp.write("\n".join(faulty_commits))
+        with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
             fp.write("faulty-commits=true")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--title", required=True, help="PR title")
 
     args = parser.parse_args()
 
-    for spack_repo in ['./var/spack/repos/builder.test',
-                       './var/spack/repos/builtin',
-                       './var/spack/repos/builtin.mock',
-                       './var/spack/repos/tutorial',
-                       './bluebrain/repo-bluebrain',
-                       './bluebrain/repo-patches']:
+    for spack_repo in [
+        "./var/spack/repos/builder.test",
+        "./var/spack/repos/builtin",
+        "./var/spack/repos/builtin.mock",
+        "./var/spack/repos/tutorial",
+        "./bluebrain/repo-bluebrain",
+        "./bluebrain/repo-patches",
+    ]:
         try:
-            EXISTING_PACKAGES.extend(next(os.walk(f'{spack_repo}/packages'))[1])
+            EXISTING_PACKAGES.extend(next(os.walk(f"{spack_repo}/packages"))[1])
         except StopIteration:
-            print(f'No packages under {spack_repo}')
+            print(f"No packages under {spack_repo}")
             pass
 
     main(args.title)
