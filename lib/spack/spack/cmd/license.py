a02cfcc55f63b27483ff2eaad5845dfd3c95fae6
Matthias Wolf <matthias.wolf@epfl.ch>
2023-03-03 18:03:36 +0100

    bump copyright year

--- lib/spack/spack/cmd/license.py
+++ lib/spack/spack/cmd/license.py
@@ -97,7 +97,7 @@ def list_files(args):
 OLD_LICENSE, SPDX_MISMATCH, GENERAL_MISMATCH = range(1, 4)
 
 #: Latest year that copyright applies. UPDATE THIS when bumping copyright.
-latest_year = 2022
+latest_year = 2023
 strict_date = r"Copyright 2013-%s" % latest_year
 
 #: regexes for valid license lines at tops of files
