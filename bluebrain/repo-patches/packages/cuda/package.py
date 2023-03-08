65585806fe9c5c9cb7d3c0c96b90a6e445745e24
Olli Lupton <oliver.lupton@epfl.ch>
2023-03-08 08:37:20 +0100

    cuda: set NVHPC_CUDA_HOME too

--- bluebrain/repo-patches/packages/cuda/package.py
+++ bluebrain/repo-patches/packages/cuda/package.py
@@ -7,3 +7,7 @@ class Cuda(BuiltinCuda):
     def setup_run_environment(self, env):
         super().setup_run_environment(env)
         env.append_path("LD_LIBRARY_PATH", self.prefix.extras.CUPTI.lib64)
+        # Avoid a warning from nvc++ when the nvhpc and cuda modules
+        # are loaded simultaneously: nvc++-Warning-CUDA_HOME has been
+        # deprecated. Please, use NVHPC_CUDA_HOME instead.
+        env.append_path("NVHPC_CUDA_HOME", self.prefix)
