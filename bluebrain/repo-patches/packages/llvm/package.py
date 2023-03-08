057624064019c39368c0832bc2cfff6927d833a9
Olli Lupton <oliver.lupton@epfl.ch>
2023-03-08 18:02:28 +0100

    llvm: run dependency on hwloc

--- bluebrain/repo-patches/packages/llvm/package.py
+++ bluebrain/repo-patches/packages/llvm/package.py
@@ -6,3 +6,5 @@ class Llvm(BuiltinLlvm):
     __doc__ = BuiltinLlvm.__doc__
     # We need a modern linker to use modern compilers
     depends_on("binutils+ld", type="run")
+    # LLVM's libomp.so doesn't have an rpath for libhwloc.so
+    depends_on("hwloc", type="run")
