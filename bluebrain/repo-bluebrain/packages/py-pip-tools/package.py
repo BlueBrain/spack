e3c1e4234626b23d764ccd98f07f81838cebcae9
Tristan Carel <tristan.carel@epfl.ch>
2023-03-08 16:24:08 +0100

    Update bluebrain/repo-bluebrain/packages/py-pip-tools/package.py
    Co-authored-by: Erik Heeren <erik.heeren@epfl.ch>

--- bluebrain/repo-bluebrain/packages/py-pip-tools/package.py
+++ bluebrain/repo-bluebrain/packages/py-pip-tools/package.py
@@ -15,7 +15,7 @@ class PyPipTools(PythonPackage):
     version("6.12.3", sha256="480d44fae6e09fad3f9bd3d0a7e8423088715d10477e8ef0663440db25e3114f")
 
     depends_on("py-click@8:", type=("build", "run"))
-    depends_on("py-setuptools@63:", type="build")
+    depends_on("py-setuptools@63:", type=("build", "run"))
     depends_on("py-setuptools-scm+toml@7:", type="build")
     depends_on("py-pip@22.2:", type=("build", "run"))
     depends_on("py-build", type=("build", "run"))
