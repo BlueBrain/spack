48f27984227dceffca4a97a1bba73d095c13137a
Nadir Román Guerrero <nadir.ro.gue@gmail.com>
2023-03-10 15:41:45 +0100

    levelzero: fix package name

--- bluebrain/repo-patches/packages/levelzero/package.py
+++ bluebrain/repo-patches/packages/levelzero/package.py
@@ -6,7 +6,7 @@
 from spack.package import *
 
 
-class LevelZero(CMakePackage):
+class Levelzero(CMakePackage):
     """
     Intel API to provide direct-to-metal interfaces to offload accelerator
     devices.
