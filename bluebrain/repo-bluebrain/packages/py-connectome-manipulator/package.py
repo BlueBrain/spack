f04fa8c1d16e657d45b5062ecd090b2bf568deb1
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2023-03-28 15:11:37 +0200

    Add parquet variant for pyarrow

--- bluebrain/repo-bluebrain/packages/py-connectome-manipulator/package.py
+++ bluebrain/repo-bluebrain/packages/py-connectome-manipulator/package.py
@@ -25,6 +25,6 @@ class PyConnectomeManipulator(PythonPackage):
     depends_on("py-scikit-learn", type=("build", "run"))
     depends_on("py-submitit", type=("build", "run"))
     depends_on("py-voxcell", type=("build", "run"))
-    depends_on("py-pyarrow", type=("build", "run"))
+    depends_on("py-pyarrow+parquet@3.0.0:", type=("build", "run"))
     depends_on("py-jsonpickle", type=("build", "run"))
     depends_on("py-submitit", type=("build", "run"))
