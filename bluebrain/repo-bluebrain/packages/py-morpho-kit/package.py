f149ca265a4c845b176060df7839d376fe92daae
Luc Grosheintz <luc.grosheintz@gmail.ch>
2023-06-09 11:09:37 +0200

    py-morpho-kit: release 0.3.5

--- bluebrain/repo-bluebrain/packages/py-morpho-kit/package.py
+++ bluebrain/repo-bluebrain/packages/py-morpho-kit/package.py
@@ -15,6 +15,7 @@ class PyMorphoKit(PythonPackage):
     submodules = True
 
     version("develop", branch="main")
+    version("0.3.5", commit="aee8232e8acb7add30e210b11fe2528fa658fb9f")
     version("0.3.4", tag="v0.3.4")
     version("0.3.3", tag="0.3.3")
     version("0.3.2", tag="v0.3.2")
@@ -23,7 +24,8 @@ class PyMorphoKit(PythonPackage):
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
 
-    depends_on("morphio@2.3.9:", type=("build", "link"))
+    depends_on("morphio@2.3.9:", type=("build", "link"), when="@0.3.4")
+    depends_on("morphio@3.3.5:", type=("build", "link"), when="@0.3.5:")
 
     depends_on("cmake@3.2:", type="build")
     depends_on("py-numpy", type="run")
