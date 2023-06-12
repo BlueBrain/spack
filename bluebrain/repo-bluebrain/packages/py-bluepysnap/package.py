fe0b64d31ee7eb4351f2388c3578da0d43c18c13
Mike Gevaert <michael.gevaert@epfl.ch>
2023-06-12 12:20:11 +0200

    review comments

--- bluebrain/repo-bluebrain/packages/py-bluepysnap/package.py
+++ bluebrain/repo-bluebrain/packages/py-bluepysnap/package.py
@@ -17,7 +17,7 @@ class PyBluepysnap(PythonPackage):
     version("develop", branch="master")
     version("1.0.6", sha256="78d76354b3076e2e6d5f2332ac8380086854c7890aad86d01d6f21a927f73198")
 
-    depends_on("python@3.7:", type=("build", "run"))
+    depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-setuptools", type=("build", "run"))
     depends_on("py-setuptools-scm", type="build")
 
@@ -28,7 +28,7 @@ class PyBluepysnap(PythonPackage):
     depends_on("py-morphio@3", type=("build", "run"))
     depends_on("py-morph-tool@2.4.3:2", type=("build", "run"))
     depends_on("py-numpy@1.8:", type=("build", "run"))
-    depends_on("py-pandas@1.0.0:1", type=("build", "run"))
+    depends_on("py-pandas@1.0.0:", type=("build", "run"))
     depends_on("py-click@7.0:", type=("build", "run"))
     depends_on("py-more-itertools@8.2.0:", type=("build", "run"))
     depends_on("spatial-index@1.2.1:", type=("build", "run"))
