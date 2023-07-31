bca14cb2fabb5f9fe42fe46967c8d749a40b5c6e
Erik Heeren <erik.heeren@epfl.ch>
2023-07-31 13:36:40 +0200

    Switch to a non-relevant package

--- var/spack/repos/builtin/packages/singularityce/package.py
+++ var/spack/repos/builtin/packages/singularityce/package.py
@@ -12,7 +12,6 @@ from spack.package import *
 
 
 class SingularityBase(MakefilePackage):
-    # comment to trigger build
     variant("suid", default=True, description="install SUID binary")
     variant("network", default=True, description="install network plugins")
 
