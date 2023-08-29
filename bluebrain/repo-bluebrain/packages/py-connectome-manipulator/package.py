91c8c23006eaa48c048b1581220543e8fe50c52f
Omar Awile <omar.awile@epfl.ch>
2023-08-29 16:57:16 +0200

    py-connectome-manipulator: Bump version to 0.0.7

--- bluebrain/repo-bluebrain/packages/py-connectome-manipulator/package.py
+++ bluebrain/repo-bluebrain/packages/py-connectome-manipulator/package.py
@@ -14,6 +14,7 @@ class PyConnectomeManipulator(PythonPackage):
     git = "ssh://git@bbpgitlab.epfl.ch/conn/structural/connectome_manipulator.git"
 
     version("develop", branch="main")
+    version("0.0.7", tag="connectome-manipulator-v0.0.7")
     version("0.0.6", tag="connectome-manipulator-v0.0.6")
     version("0.0.4", tag="connectome-manipulator-v0.0.4")
 
