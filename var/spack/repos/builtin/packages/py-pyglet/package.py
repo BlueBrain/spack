0410b621f7b016ff15b049764ad57a899133ee70
Adrien Berchet <adrien.berchet@gmail.com>
2023-09-28 16:22:12 +0200

    Try to use the wheel of pyglet

--- var/spack/repos/builtin/packages/py-pyglet/package.py
+++ var/spack/repos/builtin/packages/py-pyglet/package.py
@@ -12,9 +12,13 @@ class PyPyglet(PythonPackage):
     """
 
     homepage = "https://github.com/pyglet/pyglet"
-    pypi = "pyglet/pyglet-2.0.9.zip"
+    pypi = "pyglet/pyglet-2.0.9-py3-none-any.whl"
 
-    version("2.0.9", sha256="a0922e42f2d258505678e2f4a355c5476c1a6352c3f3a37754042ddb7e7cf72f")
+    version(
+        "2.0.9",
+        sha256="8520b22dde75f47167e1fedeed58ac0bb0c890c0dca17d8528427d6b318cd9cc",
+        expand=False,
+    )
     version("1.4.2", sha256="fda25ae5e99057f05bd339ea7972196d2f44e6fe8fb210951ab01f6609cdbdb7")
     version("1.2.1", sha256="d1afb253d6de230e73698377566da333ef42e1c82190216aa7a0c1b729d6ff4d")
 
