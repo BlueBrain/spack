858f96daf5c48b5098a2a2e31575c2875d97a71a
Erik Heeren <erik.heeren@epfl.ch>
2023-10-17 08:50:00 +0200

    py-currentscape: make Black happy

--- var/spack/repos/builtin/packages/py-currentscape/package.py
+++ var/spack/repos/builtin/packages/py-currentscape/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -20,7 +20,7 @@ class PyCurrentscape(PythonPackage):
 
     depends_on("py-setuptools", type=("build", "run"))
     depends_on("py-setuptools-scm", type=("build",))
-    depends_on("python@3.8:", type=("build", "run",))
+    depends_on("python@3.8:", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
     depends_on("py-palettable", type=("build", "run"))
