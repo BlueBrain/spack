2ac4429cc985f29fba858d011e46afaae1bfb7c0
Adrien Berchet <adrien.berchet@gmail.com>
2023-11-06 17:43:23 +0100

    py-atlas-analysis: fix vtk

--- bluebrain/repo-bluebrain/packages/py-atlas-analysis/package.py
+++ bluebrain/repo-bluebrain/packages/py-atlas-analysis/package.py
@@ -12,6 +12,7 @@ class PyAtlasAnalysis(PythonPackage):
     version("develop", branch="main")
     version("0.0.6", tag="atlas-analysis-v0.0.6")
 
+    depends_on("nlohmann-json", type=("build", "run"))
     depends_on("py-click@7.0:", type=("build", "run"))
     depends_on("py-geomdl@5.2.8:", type=("build", "run"))
     depends_on("py-numpy@1.16.3:", type=("build", "run"))
@@ -22,4 +23,4 @@ class PyAtlasAnalysis(PythonPackage):
     depends_on("py-scikit-image@0.16.1:", type=("build", "run"))
     depends_on("py-scipy@1.2.1:", type=("build", "run"))
     depends_on("py-voxcell@2.6.2:", type=("build", "run"))
-    depends_on("vtk@8.1.2: +python", type=("build", "run"))
+    depends_on("vtk+python@8.1.2:", type=("build", "run"))
