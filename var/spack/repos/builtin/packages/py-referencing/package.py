f46bcf17b5ed6fe21467be71bac17fc3f09680e4
Joni Herttuainen <joni.herttuainen@epfl.ch>
2023-11-07 10:00:11 +0100

    add package: referencing

---
+++ var/spack/repos/builtin/packages/py-referencing/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.package import *
+
+
+class PyReferencing(PythonPackage):
+    """An implementation-agnostic implementation of JSON reference resolution."""
+
+    homepage = "https://github.com/python-jsonschema/referencing"
+    pypi = "referencing/referencing-0.30.2.tar.gz"
+
+    version("0.30.2", sha256="794ad8003c65938edcdbc027f1933215e0d0ccc0291e3ce20a4d87432b59efc0")
+
+    depends_on("py-setuptools", type="build")
