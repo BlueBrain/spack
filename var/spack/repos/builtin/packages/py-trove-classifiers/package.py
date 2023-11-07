5ca4f019e0003eb1616decf04589d2e14152cc98
Joni Herttuainen <joni.herttuainen@epfl.ch>
2023-11-07 13:37:00 +0100

    add setuptools as dependency to trove-classifier build

--- var/spack/repos/builtin/packages/py-trove-classifiers/package.py
+++ var/spack/repos/builtin/packages/py-trove-classifiers/package.py
@@ -13,3 +13,5 @@ class PyTroveClassifiers(PythonPackage):
     pypi = "trove-classifiers/trove-classifiers-2023.10.18.tar.gz"
 
     version("2023.10.18", sha256="2cdfcc7f31f7ffdd57666a9957296089ac72daad4d11ab5005060e5cd7e29939")
+
+    depends_on("py-setuptools", type="build")
