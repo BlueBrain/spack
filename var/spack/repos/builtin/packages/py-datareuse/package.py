b39787dab97b308a92780e1e8d05f596be28e4ae
arnaudon <alexis.arnaudon@epfl.ch>
2023-12-01 15:10:58 +0100

    tried somehting

--- var/spack/repos/builtin/packages/py-datareuse/package.py
+++ var/spack/repos/builtin/packages/py-datareuse/package.py
@@ -14,5 +14,7 @@ class PyDatareuse(PythonPackage):
 
     version("0.0.3", sha256="63b03ae4acda22e93911adde0325818842bf65e42291ecfc88706e735a190749")
 
+    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools-scm", type="build")
     depends_on("py-pandas@1.0.2:", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"))
