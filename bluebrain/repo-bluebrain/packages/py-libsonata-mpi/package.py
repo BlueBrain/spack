b8541479e9275534f25fa72e105e7d5ecd67a001
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-01-15 11:35:25 +0100

    py-libsonata-mpi: prepare v0.0.2

--- bluebrain/repo-bluebrain/packages/py-libsonata-mpi/package.py
+++ bluebrain/repo-bluebrain/packages/py-libsonata-mpi/package.py
@@ -14,15 +14,14 @@ class PyLibsonataMpi(PythonPackage):
     url = "https://github.com/BlueBrain/libsonata-mpi-v0.0.0.tar.gz"
 
     version("develop", branch="main", get_full_repo=True, submodules=True)
+
+    version("0.0.1.1", commit="a9c97df578f762f286fad3555e4997ccce5d87b1")
     version("0.0.1", tag="0.0.1", submodules=True)
 
     depends_on("cmake@3.18:", type="build")
 
     depends_on("py-mpi4py", type=("build", "link", "run"))
-    depends_on("highfive+mpi", type=("build", "link"))
-    depends_on("py-libsonata@0.1.24.1:", type="run")
-
-    depends_on("py-pybind11@2.11.1:")
+    depends_on("py-libsonata@0.1.25:", type="run")
 
     depends_on("py-setuptools", type="build")
     depends_on("py-setuptools-scm", type="build")
