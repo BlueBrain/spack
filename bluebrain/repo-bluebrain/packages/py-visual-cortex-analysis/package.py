ddf329fea20b46834fd17e28583e9799e41a286e
Dictus Hugo <hugo.dictus@epfl.ch>
2024-02-12 17:41:45 +0100

    increase py-visual-cortex-analysis version

--- bluebrain/repo-bluebrain/packages/py-visual-cortex-analysis/package.py
+++ bluebrain/repo-bluebrain/packages/py-visual-cortex-analysis/package.py
@@ -12,7 +12,7 @@ class PyVisualCortexAnalysis(PythonPackage):
     homepage = "https://bbpgitlab.epfl.ch/circuits/proj120/visual-cortex-analysis"
     git = "ssh://git@bbpgitlab.epfl.ch/circuits/proj120/visual-cortex-analysis.git"
 
-    version("0.1.0.dev0", tag="visual-cortex-analysis-v0.1.0.dev0")
+    version("0.2", tag="visual-cortex-analysis-v0.2")
 
     depends_on("py-setuptools", type="build")
 
