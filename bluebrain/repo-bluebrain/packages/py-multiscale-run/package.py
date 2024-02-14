22fe0dd61ea2d89ad510558a79136d316fed1d8b
katta <alessandro.cattabiani@epfl.ch>
2024-02-14 10:10:25 +0100

    py-multiscale-run: v 0.5.1, 0.5

--- bluebrain/repo-bluebrain/packages/py-multiscale-run/package.py
+++ bluebrain/repo-bluebrain/packages/py-multiscale-run/package.py
@@ -15,6 +15,8 @@ class PyMultiscaleRun(PythonPackage):
     maintainers("tristan0x", "cattabiani")
 
     version("develop", branch="main")
+    version("0.5.1", tag="0.5.1")
+    version("0.5", tag="0.5")
     version("0.4", tag="0.4")
     version("0.3", tag="0.3")
     version("0.2", tag="0.2")
