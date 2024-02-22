e30ffe0590415f84466e8b32f553f33122befa03
Erik Heeren <erik.heeren@epfl.ch>
2024-02-22 14:55:06 +0100

    py-find-libpython: license directive

--- var/spack/repos/builtin/packages/py-find-libpython/package.py
+++ var/spack/repos/builtin/packages/py-find-libpython/package.py
@@ -12,6 +12,8 @@ class PyFindLibpython(PythonPackage):
     homepage = "https://github.com/ktbarrett/find_libpython"
     pypi = "find_libpython/find_libpython-0.3.0.tar.gz"
 
+    license("MIT")
+
     version("0.3.0", sha256="6e7fe5d9af7fad6dc066cb5515a0e9c90a71f1feb2bb2f8e4cdbb4f83276e9e5")
 
     depends_on("py-setuptools@43:", type="build")
