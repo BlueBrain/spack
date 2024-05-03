812776c8035466c8cd108258580e9d0b028f0dfa
Mike Gevaert <michael.gevaert@epfl.ch>
2024-05-03 13:20:01 +0200

    py-annotated-types,py-pydantic: added/updated

---
+++ var/spack/repos/builtin/packages/py-annotated-types/package.py
@@ -0,0 +1,10 @@
+from spack.package import *
+
+class PyAnnotatedTypes(PythonPackage):
+    """Reusable constraint types to use with typing.Annotated"""
+    homepage = "https://www.example.com"
+    pypi = "annotated-types/annotated_types-0.6.0.tar.gz"
+
+    version("0.6.0", sha256="563339e807e53ffd9c267e99fc6d9ea23eb8443c08f112651963e24e22f84a5d")
+
+    depends_on("py-hatchling@0.15.0:", type="build")
