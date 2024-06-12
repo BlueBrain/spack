1c907696df52686ab4ad2aaa9719fa18dfd91645
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2024-06-12 12:26:46 +0200

    brayns, py-brayns: Release 3.8.1. (#2457)

--- bluebrain/repo-bluebrain/packages/py-brayns/package.py
+++ bluebrain/repo-bluebrain/packages/py-brayns/package.py
@@ -18,8 +18,8 @@ class PyBrayns(PythonPackage):
     build_directory = "python"
 
     version("develop", branch="develop")
-    version("3.7.2", tag="3.7.2")
     version("3.8.0", tag="3.8.0")
+    version("3.8.1", tag="3.8.1")
 
     depends_on("py-setuptools", type=("build"))
     depends_on("py-websockets@10.3:", type=("build", "run"))
