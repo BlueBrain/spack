9247f936024d68504dcbe3bc81a1048ff850627a
Adrien4193 <adrien.fleury93@gmail.com>
2024-06-25 08:37:44 +0200

    brayns, py-brayns: release 3.8.2.

--- bluebrain/repo-bluebrain/packages/brayns/package.py
+++ bluebrain/repo-bluebrain/packages/brayns/package.py
@@ -17,6 +17,7 @@ class Brayns(CMakePackage):
     version("develop", branch="develop")
     version("3.8.0", tag="3.8.0")
     version("3.8.1", tag="3.8.1")
+    version("3.8.2", tag="3.8.2")
 
     depends_on("cmake@3.15:", type="build")
     depends_on("ispc@1.18.0:", type="build")
