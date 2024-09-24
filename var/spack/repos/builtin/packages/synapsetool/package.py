99c2b62fad0b17b24ef9769b2c191965d971eb42
Matthias Wolf <matthias.wolf@epfl.ch>
2024-09-24 15:27:26 +0200

    synapsetool: add bogus version.
    Use `builtin` namespace to automatically delete it.

---
+++ var/spack/repos/builtin/packages/synapsetool/package.py
@@ -0,0 +1,13 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Synapsetool(BundlePackage):
+    """FIXME: Delete this entire package."""
+
+    homepage = "https://www.example.com"
+    license("UNKNOWN")
