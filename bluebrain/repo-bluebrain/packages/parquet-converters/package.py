3b0f5f4acc9b462653c1ba49def38ffeadc6bcd6
Matthias Wolf <matthias.wolf@epfl.ch>
2024-09-24 13:44:03 +0200

    parquet-converters, synapsetool: bump version, change URL, retire synapsetool

--- bluebrain/repo-bluebrain/packages/parquet-converters/package.py
+++ bluebrain/repo-bluebrain/packages/parquet-converters/package.py
@@ -9,34 +9,25 @@ from spack.package import *
 class ParquetConverters(CMakePackage):
     """Parquet conversion tools developed by Blue Brain Project, EPFL"""
 
-    homepage = "https://bbpgitlab.epfl.ch/hpc/circuit-building/parquet-converters"
-    url = "ssh://git@bbpgitlab.epfl.ch/hpc/circuit-building/parquet-converters.git"
-    git = "ssh://git@bbpgitlab.epfl.ch/hpc/circuit-building/parquet-converters.git"
+    homepage = "https://github.com/BlueBrain/parquet-converters"
+    git = "https://github.com/BlueBrain/parquet-converters.git"
 
     submodules = True
 
     version("develop", branch="main")
-    version("0.8.1", tag="v0.8.1")
-    version("0.8.0", tag="v0.8.0")
-    version("0.7.0", tag="v0.7.0")
-    version("0.6.1", tag="v0.6.1")
-    version("0.5.7", tag="v0.5.7")
+    version("0.9.0", tag="v0.9.0")
 
-    depends_on("hdf5+mpi")
-    depends_on("highfive+mpi")
-    depends_on("arrow+parquet+snappy@:0.12", when="@:0.5.5")
-    depends_on("arrow+parquet+snappy@0.15.1", when="@0.5.6:0.5.7")
-    depends_on("arrow+parquet+snappy@3.0.0:", when="@0.6.0:")
-    depends_on("nlohmann-json", when="@0.7.1:")
-    depends_on("synapsetool+mpi", when="@:0.8")
     depends_on("mpi")
-    depends_on("range-v3@:0.10", when="@0.4:0.8")
-    depends_on("range-v3@0.11:", when="@0.9:")
-    depends_on("catch2", when="@0.9:")
+
+    depends_on("arrow+parquet+snappy@3.0.0:")
+    depends_on("catch2", type="build")
+    depends_on("hdf5+mpi")
+    depends_on("highfive+mpi", type="build")
+    depends_on("nlohmann-json", type="build")
+    depends_on("range-v3@0.11:", type="build")
 
     def cmake_args(self):
         return [
             self.define("CMAKE_C_COMPILER", self.spec["mpi"].mpicc),
             self.define("CMAKE_CXX_COMPILER", self.spec["mpi"].mpicxx),
-            self.define("NEURONPARQUET_USE_MPI", True),
         ]
