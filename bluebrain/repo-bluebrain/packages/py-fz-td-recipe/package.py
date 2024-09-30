52b028f8c90a944f87301d5830130fecbf9916d4
Matthias Wolf <matthias.wolf@epfl.ch>
2024-09-30 11:25:36 +0200

    py-fz-td-recipe: bump version. (#2513)

--- bluebrain/repo-bluebrain/packages/py-fz-td-recipe/package.py
+++ bluebrain/repo-bluebrain/packages/py-fz-td-recipe/package.py
@@ -15,6 +15,7 @@ class PyFzTdRecipe(PythonPackage):
     pypi = "fz-td-recipe/fz_td_recipe-0.2.2.tar.gz"
 
     version("develop", branch="main")
+    version("0.2.4", sha256="e902ca32b7c94306adbd0763dbf38b39fec4e4fdb41bc3531d4c3030a23a3004")
     version("0.2.2", sha256="34eded0440c065c5a3fabe43ed0c736a6b4174b8f6ac24a4275818954d6cabda")
     version("0.1.2", tag="fz-td-recipe-v0.1.2")
 
@@ -24,7 +25,8 @@ class PyFzTdRecipe(PythonPackage):
 
     depends_on("py-click", type=("build", "run"), when="@0.2:")
     depends_on("py-jsonschema", type=("build", "run"), when="@0.2:")
-    depends_on("py-lxml@:4", type=("build", "run"))
+    depends_on("py-lxml@:4", type=("build", "run"), when="@:0.2.2")
+    depends_on("py-lxml@5:", type=("build", "run"), when="@0.2.4:")
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-pandas", type=("build", "run"))
     depends_on("py-pyyaml", type=("build", "run"), when="@0.2.2:")
