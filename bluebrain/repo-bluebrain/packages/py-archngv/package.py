eb590515f746e4b32434b9e9444ae3524cc4bb85
Matthias Wolf <matthias.wolf@epfl.ch>
2024-10-09 12:56:10 +0200

    spatial-index: equvialent of `apt-get purge`

--- bluebrain/repo-bluebrain/packages/py-archngv/package.py
+++ bluebrain/repo-bluebrain/packages/py-archngv/package.py
@@ -10,46 +10,46 @@ class PyArchngv(PythonPackage):
     """Building workflow and circuit API for Neuro-Glia-Vascular circuits."""
 
     homepage = "https://bbpgitlab.epfl.ch/nse/ArchNGV"
-    git = "ssh://git@bbpgitlab.epfl.ch/nse/ArchNGV.git"
+    pypi = "archngv/archngv-3.3.0.tar.gz"
 
-    version("develop", branch="main")
-    version("3.1.0", tag="ArchNGV-v3.1.0")
+    version("3.3.0", sha256="dc7a909793b189b5ff44e4c27de77203e69ecfcc25607de4d4e33f553f738428")
 
     depends_on("py-setuptools@42:", type="build")
 
     depends_on("py-numpy@1.22:", type=("build", "run"))
-    depends_on("py-scipy@1.5.0:", type=("build", "run"))
     depends_on("py-h5py@3.1.0:", type=("build", "run"))
+    depends_on("py-scipy@1.5.0:", type=("build", "run"))
     depends_on("py-libsonata@0.1.21:", type=("build", "run"))
-    depends_on("py-bluepysnap@1.0.5:1", type=("build", "run"))
+    depends_on("py-bluepysnap@1.0:", type=("build", "run"))
     depends_on("py-cached-property@1.5:", type=("build", "run"))
     depends_on("py-voxcell@3.0.0:", type=("build", "run"))
     depends_on("py-vascpy@0.1.0:", type=("build", "run"))
-    depends_on("py-atlas-commons@0.1.4:", type=("build", "run"))
+    depends_on("py-trimesh@2.38.10:", type=("build", "run"))
+    # needed for trimesh marchingcubes
+    depends_on("py-scikit-image", type=("build", "run"))
 
-    depends_on("py-ngv-ctools@1.0:", type=("build", "run"))
-    depends_on("spatial-index@2.0.0:", type=("build", "run"))
-    depends_on("py-bluepy-configfile@0.1.11:", type=("build", "run"))
+    # "all" optional dependencies
+    depends_on("py-ngv-ctools@1.0.0:", type=("build", "run"))
     depends_on("py-click@7.0:", type=("build", "run"))
     depends_on("py-numpy-stl@2.10", type=("build", "run"))
     depends_on("py-openmesh@1.1.2:", type=("build", "run"))
     depends_on("py-pyyaml@5.0:", type=("build", "run"))
     depends_on("py-pandas@1.1.0:", type=("build", "run"))
-    depends_on("py-tess@0.3.2", type=("build", "run"))
-    depends_on("py-morphio@3.3.1:", type=("build", "run"))
-    depends_on("py-pytouchreader@1.4.7:", type=("build", "run"))
+    depends_on("py-multivoro", type=("build", "run"))
+    depends_on("py-morphio@3.2.0:", type=("build", "run"))
     depends_on("py-morph-tool@2.4.0:", type=("build", "run"))
     depends_on("snakemake@5.0:", type=("build", "run"))
     depends_on("py-tmd@2.0.11:", type=("build", "run"))
     depends_on("py-neurots@3.4.0:", type=("build", "run"))
     depends_on("py-diameter-synthesis@0.5.4", type=("build", "run"))
-    depends_on("py-trimesh@2.38.10:", type=("build", "run"))
-    # needed for trimesh marchingcubes
-    depends_on("py-scikit-image", type=("build", "run"))
 
     depends_on("py-dask+distributed@2022.04.1:", type=("build", "run"))
     depends_on("py-distributed@2.0:", type=("build", "run"))
     depends_on("py-dask-mpi@2.0:", type=("build", "run"))
+
+    depends_on("py-brain-indexer@3.0.0:", type=("build", "run"))
+    depends_on("py-atlas-commons@0.1.4:", type=("build", "run"))
+    depends_on("py-meshio@5.3.4", type=("build", "run"))
 
     depends_on("py-pytest", type="test")
 
