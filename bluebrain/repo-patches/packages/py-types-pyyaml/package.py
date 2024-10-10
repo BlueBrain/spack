5fb2fc538512169db596df1024404dc2c3ddb53b
Matthias Wolf <matthias.wolf@epfl.ch>
2024-10-10 14:49:48 +0200

    ❄️

--- bluebrain/repo-patches/packages/py-types-pyyaml/package.py
+++ bluebrain/repo-patches/packages/py-types-pyyaml/package.py
@@ -12,7 +12,10 @@ class PyTypesPyyaml(PythonPackage):
     homepage = "https://github.com/python/typeshed"
     pypi = "types-pyyaml/types-PyYAML-5.4.6.tar.gz"
 
-    version("6.0.12.20240917", sha256="d1405a86f9576682234ef83bcb4e6fff7c9305c8b1fbad5e0bcd4f7dbdc9c587")
+    version(
+        "6.0.12.20240917",
+        sha256="d1405a86f9576682234ef83bcb4e6fff7c9305c8b1fbad5e0bcd4f7dbdc9c587",
+    )
     version("5.4.6", sha256="745dcb4b1522423026bcc83abb9925fba747f1e8602d902f71a4058f9e7fb662")
 
     depends_on("py-setuptools", type=("build"))
