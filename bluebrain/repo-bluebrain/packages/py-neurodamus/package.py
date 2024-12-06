ea4e60020c6344ff0709bafa653201567a66f964
Weina Ji <weina.ji@epfl.ch>
2024-12-06 15:19:18 +0100

    py-docopt-ng

--- bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
+++ bluebrain/repo-bluebrain/packages/py-neurodamus/package.py
@@ -65,7 +65,7 @@ class PyNeurodamus(PythonPackage):
     depends_on("python@3.4:", type=("build", "run"))
     depends_on("py-h5py", type=("build", "run"))
     depends_on("py-numpy@1.24:", type=("build", "run"))
-    depends_on("py-docopt", type=("build", "run"))
+    depends_on("py-docopt-ng", type=("build", "run"))
     depends_on("py-libsonata@:0.1.27", when="@:3.5.0", type=("build", "run"))
     depends_on("py-libsonata", type=("build", "run"))  # else
     depends_on("py-morphio", type=("build", "run"))
